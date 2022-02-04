7f1305709d12b6e3e50b2ecf8e1af6b1a6f58be9
2Dokk <2dokyoung@gmail.com>
2022-02-04 17:53:54 +0900

    calculator

--- test.js
+++ test.js
@@ -1,19 +1,17 @@
 "use strict";
-let calculator = {
-    read() {
-        let num1 = +prompt('첫번째 숫자를 입력하세요','');
-        let num2 = +prompt('두번째 숫자를 입력하세요','');
-        return this.Num1 = num1, this.Num2 = num2;
-    },
-    sum() {
-        let sum = this.Num1 + this.Num2;
-        return sum;
-    },
-    mul() {
-        let mul = this.Num1 * this.Num2;
-        return mul;
+function Calculator() {
+    this.read = function() {
+        this.Num1 = +prompt('First Number',0);
+        this.Num2 = +prompt('Second Number',0);
+    }
+    this.sum = function() {
+        return this.Num1 + this.Num2;
+    }
+    this.mul = function() {
+        return this.Num1 * this.Num2;
     }
 };
+let calculator = new Calculator();
 calculator.read();
-alert( calculator.sum() );
-alert( calculator.mul() );
+alert('Sum=' + calculator.sum());
+alert('Mul=' + calculator.mul());
