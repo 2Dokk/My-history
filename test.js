b6bcdd48f0d5f491bf4b6b6a9ea00d265eefe77e
2Dokk <2dokyoung@gmail.com>
2022-02-04 14:33:18 +0900

    this

--- test.js
+++ test.js
@@ -1,12 +1,19 @@
 "use strict";
-user  = {
-    sayHi: function() {
-        alert('Hello');
+let calculator = {
+    read() {
+        let num1 = +prompt('첫번째 숫자를 입력하세요','');
+        let num2 = +prompt('두번째 숫자를 입력하세요','');
+        return this.Num1 = num1, this.Num2 = num2;
+    },
+    sum() {
+        let sum = this.Num1 + this.Num2;
+        return sum;
+    },
+    mul() {
+        let mul = this.Num1 * this.Num2;
+        return mul;
     }
 };
-
-user = {
-    sayHi(){
-        alert('Hello');
-    }
-};
+calculator.read();
+alert( calculator.sum() );
+alert( calculator.mul() );
