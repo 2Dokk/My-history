78dba9de8a7b16246070e6c2ab7ac1629c42e06d
2Dokk <2dokyoung@gmail.com>
2022-02-05 18:17:36 +0900

    20220205

--- test.js
+++ test.js
@@ -1,12 +1,5 @@
 "use strict";
-function Accumulator(startingValue) {
-    this.value = startingValue,
-    this.read = function() {
-        return this.value += +prompt('더할 값을 입력해주십시오',0);
-    };
-};
-let accumulator = new Accumulator(1);
-accumulator.read();
-accumulator.read();
+let a = +prompt("The first number?", "");
+let b = +prompt("The second number?", "");
 
-alert(accumulator.value);
+alert( a + b );
