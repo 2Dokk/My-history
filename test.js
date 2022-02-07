2cbfa9420a603aca888675922d6483aa6b0869d4
2Dokk <2dokyoung@gmail.com>
2022-02-07 11:18:43 +0900

    20220207

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 "use strict";
-let guestList = `손님:
-* John
-* Pete
-* Mary
-`;
-alert(guestList);
+let str = "As sly as a fox, as strong as an ox";
+let target = "as";
+
+let pos = -1;
+while ((pos = str.indexOf(target, pos + 1)) != -1) {
+  alert( `위치: ${pos}` );
+}
