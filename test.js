936c2c224d3f91817cbd3271f7552425d37bd60f
2Dokk <2dokyoung@gmail.com>
2022-02-09 01:04:24 +0900

    20220208

--- test.js
+++ test.js
@@ -1,20 +1,14 @@
 "use strict";
-function filterRangeInPlace(arr, a, b) {
+let arr = ["HTML", "JavaScript", "CSS"];
 
-    for (let i = 0; i < arr.length; i++) {
-      let val = arr[i];
-  
-      // 범위 밖의 요소를 제거함
-      if (val < a || val > b) {
-        arr.splice(i, 1);
-        i--;
-      }
-    }
-  
-  }
-  
-  let arr = [5, 3, 8, 1];
-  
-  filterRangeInPlace(arr, 1, 4); // 1과 4 사이에 있지 않은 요소는 모두 제거함
-  
-  alert( arr ); // [3, 1]
+function copySorted(arr){
+    let copyOrigin = arr.slice()
+    copyOrigin.sort();
+    return copyOrigin;
+}
+
+let sorted = copySorted(arr);
+console.log(copySorted(arr));
+
+alert( sorted ); // CSS, HTML, JavaScript
+alert( arr ); // HTML, JavaScript, CSS (no changes)
