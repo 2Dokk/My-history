18e470e5c8cd7fd79fb0408715f87a192d7f6049
2Dokk <2dokyoung@gmail.com>
2022-02-08 17:30:48 +0900

    20220208

--- test.js
+++ test.js
@@ -1,8 +1,17 @@
 "use strict";
-let arr = ['a','b'];
-
-arr.push(function() {
-    alert(this);
-})
-
-arr[2]();
+function sumInput(){
+    let numbers = [];
+    while (true){
+        let value = prompt('숫자를 입력해주세요',0);
+        
+        if (value === ''|| value === null || !isFinite(value)) break;
+        
+        numbers.push(+value);
+    }
+    let sum = 0;
+    for (let number of numbers)  {
+        sum += number;
+    }
+    return sum;
+}
+alert(sumInput());
