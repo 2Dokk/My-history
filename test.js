dc0bc311e8ee7c0a0aaac06328c96e621c145c52
2Dokk <2dokyoung@gmail.com>
2022-02-08 23:38:04 +0900

    update

--- test.js
+++ test.js
@@ -1,20 +1,14 @@
 "use strict";
-function getMaxSubSum(arr) {
-    let maxSum = 0;
-    let partialSum = 0;
-  
-    for (let item of arr) { // 배열의 각 요소를
-      partialSum += item; // partialSum에 더합니다.
-      maxSum = Math.max(maxSum, partialSum); // 최대값을 기억해 놓습니다.
-      if (partialSum < 0) partialSum = 0; // 음수가 되면 0을 대입합니다.
-    }
-  
-    return maxSum;
+function camelize(str) {
+    return str
+      .split('-') // splits 'my-long-word' into array ['my', 'long', 'word']
+      .map(
+        // capitalizes first letters of all array items except the first one
+        // converts ['my', 'long', 'word'] into ['my', 'Long', 'Word']
+        (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+      )
+      .join(''); // joins ['my', 'Long', 'Word'] into 'myLongWord'
   }
-  
-  alert( getMaxSubSum([-1, 2, 3, -9]) ); // 5
-  alert( getMaxSubSum([-1, 2, 3, -9, 11]) ); // 11
-  alert( getMaxSubSum([-2, -1, 1, 2]) ); // 3
-  alert( getMaxSubSum([100, -9, 2, -3, 5]) ); // 100
-  alert( getMaxSubSum([1, 2, 3]) ); // 6
-  alert( getMaxSubSum([-1, -2, -3]) ); // 0
+camelize("background-color");
+camelize("list-style-image");
+camelize("-webkit-transition");
