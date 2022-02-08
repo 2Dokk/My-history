a9f37c6da59f3335fb963bf71954d5373b2849b1
2Dokk <2dokyoung@gmail.com>
2022-02-08 17:19:23 +0900

    20220208

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 "use strict";
-let styles = ["Jazz", "Blues"];
-styles.push("Rock-n-Roll");
-styles[Math.floor((styles.length - 1) / 2)] = "Classics";
-alert( styles.shift() );
-styles.unshift("Rap", "Reggae");
+let arr = ['a','b'];
+
+arr.push(function() {
+    alert(this);
+})
+
+arr[2]();
