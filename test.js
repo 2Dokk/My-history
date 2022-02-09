480ede0a7b19e980eac98ebd5aa822052829cfcd
2Dokk <2dokyoung@gmail.com>
2022-02-10 00:24:01 +0900

    20220210

--- test.js
+++ test.js
@@ -1,10 +1,15 @@
 "use strict";
-let john = { name: "John" };
-
-// 고객의 가게 방문 횟수를 세본다고 가정해 봅시다.
-let visitsCountMap = new Map();
-
-// john을 맵의 키로 사용하겠습니다.
-visitsCountMap.set(john, 123);
-
-alert( visitsCountMap.get(john) ); // 123
+function unique(arr) {
+    /* 제출 답안 */
+    let result = new Set()
+    for (let item of arr){
+        result.add(item);
+    }
+    return Array.from(result);
+  }
+  
+  let values = ["Hare", "Krishna", "Hare", "Krishna",
+    "Krishna", "Krishna", "Hare", "Hare", ":-O"
+  ];
+  
+  alert( unique(values) ); // 얼럿창엔 `Hare, Krishna, :-O`만 출력되어야 합니다.
