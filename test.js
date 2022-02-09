3be9da458d80426add639404dcbd9d7d161a6f06
2Dokk <2dokyoung@gmail.com>
2022-02-09 10:54:10 +0900

    sort

--- test.js
+++ test.js
@@ -1,22 +1,17 @@
 "use strict";
-let john = { name: "John", surname: "Smith", id: 1 };
-let pete = { name: "Pete", surname: "Hunt", id: 2 };
-let mary = { name: "Mary", surname: "Key", id: 3 };
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
 
-let users = [ john, pete, mary ];
+let arr = [ pete, john, mary ];
 
-let usersMapped = users.map(user => ({
-  fullName: `${user.name} ${user.surname}`,
-  id: user.id
-}));
+function sortByAge(arr){
+    arr.sort((a,b) => a.age - b.age);
+}
 
-/*
-usersMapped = [
-  { fullName: "John Smith", id: 1 },
-  { fullName: "Pete Hunt", id: 2 },
-  { fullName: "Mary Key", id: 3 }
-]
-*/
+sortByAge(arr);
 
-alert( usersMapped[0].id ); // 1
-alert( usersMapped[0].fullName ); // John Smith
+// now: [john, mary, pete]
+alert(arr[0].name); // John
+alert(arr[1].name); // Mary
+alert(arr[2].name); // Pete
