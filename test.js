23226c48c9d7d944272e7ef991ef9352bff2dab1
2Dokk <2dokyoung@gmail.com>
2022-02-09 20:39:01 +0900

    map_can_use_it_as_key

--- test.js
+++ test.js
@@ -1,14 +1,10 @@
 "use strict";
-function shuffle(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      let j = Math.floor(Math.random() * (i + 1)); // 무작위 인덱스(0 이상 i 미만)
-  
-      // array[i]와 array[j]를 바꿔치기합니다.
-      // 아래 답안에선 "구조 분해 할당(destructuring assignment)"이라 불리는 문법을 사용하여
-      // 원하는 것을 구현하였는데,
-      // 이 문법에 대한 자세한 내용은 이후 챕터에서 다룰 예정입니다.
-      // 구조 분해 할당을 사용하지 않고 작성한 코드는 아래와 같습니다.
-      // let t = array[i]; array[i] = array[j]; array[j] = t
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-  }
+let john = { name: "John" };
+
+// 고객의 가게 방문 횟수를 세본다고 가정해 봅시다.
+let visitsCountMap = new Map();
+
+// john을 맵의 키로 사용하겠습니다.
+visitsCountMap.set(john, 123);
+
+alert( visitsCountMap.get(john) ); // 123
