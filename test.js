e0f9c9dfb73f5b4e40018a41307e04520dd6b385
2doKK <2dokyoung@gmail.com>
2022-02-11 00:46:08 +0900

    20220211

--- test.js
+++ test.js
@@ -1,8 +1,14 @@
 "use strict";
-let user = new Map();
-user.set("name", "John");
-user.set("age", "30");
-
-for (let [key, value] of user) {
-  alert(`${key}:${value}`); // name:John, then age:30
-}
+let options = {
+    title: "Menu",
+    height: 200,
+    width: 100
+  };
+  
+  // title = 이름이 title인 프로퍼티
+  // rest = 나머지 프로퍼티들
+  let {title, ...rest} = options;
+  
+  // title엔 "Menu", rest엔 {height: 200, width: 100}이 할당됩니다.
+  alert(rest.height);  // 200
+  alert(rest.width);   // 100
