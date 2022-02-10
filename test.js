d5512c9e756ad9281be99154c7aa3d2f2ddf29ba
2doKK <2dokyoung@gmail.com>
2022-02-11 00:26:53 +0900

    20220211

--- test.js
+++ test.js
@@ -1,11 +1,5 @@
 "use strict";
-// 이름과 성을 요소로 가진 배열
-let arr = ["Bora", "Lee"]
+// 두 번째 요소는 필요하지 않음
+let [firstName, , title] = ["Julius", "Caesar", "Consul", "of the Roman Republic"];
 
-// 구조 분해 할당을 이용해
-// firstName엔 arr[0]을
-// surname엔 arr[1]을 할당하였습니다.
-let [firstName, surname] = arr;
-
-alert(firstName); // Bora
-alert(surname);  // Lee
+alert( title ); // Consul
