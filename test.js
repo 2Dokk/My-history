43c8600a805288b72041537480894439c0b91804
2Dokk <2dokyoung@gmail.com>
2022-02-11 00:04:50 +0900

    20220211

--- test.js
+++ test.js
@@ -1,16 +1,11 @@
 "use strict";
-let salaries = {
-    "John": 100,
-    "Pete": 300,
-    "Mary": 250
+let user = {
+    name: 'John',
+    age: 30
   };
   
-  alert( sumSalaries(salaries) ); // 650
-  
-  function sumSalaries(obj){
-      let sum = 0;
-      for (let num of Object.values(obj)){
-          sum += num;
-      }
-      return sum;
+  alert( count(user) ); // 2
+
+  function count(obj){
+      return Object.values(obj).length;
   }
