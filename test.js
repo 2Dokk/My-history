c854f658e986b94d8a08dd4deb6afcf74ae03d8b
2doKK <2dokyoung@gmail.com>
2022-02-13 22:29:29 +0900

    20220213

--- test.js
+++ test.js
@@ -1,10 +1,13 @@
 "use strict";
-let date = new Date(2019, 11, 5);  // 2019년 11월 5일
-console.log(getLocalDay(date));       // 금요일이므로, 5가 출력되어야 함
-function getLocalDay(date){
-  let day = date.getDay();
-  if (day == 0){
-    day = 7;
-  }
-  return day;
+function getDateAgo(date, days) {
+  let dateCopy = new Date(date);
+
+  dateCopy.setDate(date.getDate() - days);
+  return dateCopy.getDate();
 }
+
+let date = new Date(2015, 0, 2); // 2015년 1월 2일
+
+alert( getDateAgo(date, 1) ); // 1, (2015년 1월 1일)
+alert( getDateAgo(date, 2) ); // 31, (2014년 12월 31일)
+alert( getDateAgo(date, 365) ); // 2, (2014년 1월 2일)
