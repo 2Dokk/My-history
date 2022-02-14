d51a0cc4dbdd0d03a4568c3a6e3b7fcebe12e4b5
2doKK <2dokyoung@gmail.com>
2022-02-14 14:56:13 +0900

    20220214

--- test.js
+++ test.js
@@ -1,7 +1,1 @@
 "use strict";
-function getLastDayOfMonth(year, month){
-  let lastDay = new Date(year,month+1,0);
-  console.log(lastDay)
-  return lastDay.getDate();
-}
-console.log(getLastDayOfMonth(2012, 1));
