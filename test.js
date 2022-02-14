aa871846804d075a22e663aec1ef2edd16912c08
2doKK <2dokyoung@gmail.com>
2022-02-14 15:03:39 +0900

    20220214

--- test.js
+++ test.js
@@ -1,1 +1,17 @@
 "use strict";
+let room = {
+  number: 23
+};
+
+let meetup = {
+  title: "Conference",
+  participants: [{name: "John"}, {name: "Alice"}],
+  place: room // meetup은 room을 참조합니다
+};
+
+room.occupiedBy = meetup; // room은 meetup을 참조합니다
+
+alert( JSON.stringify(meetup, function replacer(key, value) {
+  alert(`${key}: ${value}`);
+  return (key == 'occupiedBy') ? undefined : value;
+}));
