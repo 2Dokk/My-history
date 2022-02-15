6bc5c71208d8db9e64cda5e49d653d749b1849bd
2doKK <2dokyoung@gmail.com>
2022-02-15 22:19:03 +0900

    20220215

--- test.js
+++ test.js
@@ -1,26 +1,15 @@
 "use strict";
+function showName() {
+  alert( arguments.length );
+  alert( arguments[0] );
+  alert( arguments[1] );
 
-let room = {
-  number: 23
-};
-
-let meetup = {
-  title: "Conference",
-  occupiedBy: [{name: "John"}, {name: "Alice"}],
-  place: room
-};
-
-room.occupiedBy = meetup;
-meetup.self = meetup;
-
-alert( JSON.stringify(meetup, function replacer(key, value) {
-  return (key != "" && value == meetup) ? undefined : value;
-}));
-
-/*
-{
-  "title":"Conference",
-  "occupiedBy":[{"name":"John"},{"name":"Alice"}],
-  "place":{"number":23}
+  // arguments는 이터러블 객체이기 때문에
+  // for(let arg of arguments) alert(arg); 를 사용해 인수를 펼칠 수 있습니다.
 }
-*/
+
+// 2, Bora, Lee가 출력됨
+showName("Bora", "Lee");
+
+// 1, Bora, undefined가 출력됨(두 번째 인수는 없음)
+showName("Bora");
