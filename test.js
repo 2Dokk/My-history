18b91744f7f4de3859965660092a8426d0ec02be
2doKK <2dokyoung@gmail.com>
2022-02-15 22:30:10 +0900

    20220216

--- test.js
+++ test.js
@@ -1,15 +1,4 @@
 "use strict";
-function showName() {
-  alert( arguments.length );
-  alert( arguments[0] );
-  alert( arguments[1] );
+let str = "Hello";
 
-  // arguments는 이터러블 객체이기 때문에
-  // for(let arg of arguments) alert(arg); 를 사용해 인수를 펼칠 수 있습니다.
-}
-
-// 2, Bora, Lee가 출력됨
-showName("Bora", "Lee");
-
-// 1, Bora, undefined가 출력됨(두 번째 인수는 없음)
-showName("Bora");
+alert( [...str] ); // H,e,l,l,o
