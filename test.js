3112841504ebdc797fb11d86ee9e5cafbda9478c
2doKK <2dokyoung@gmail.com>
2022-02-21 22:10:00 +0900

    20220221

--- test.js
+++ test.js
@@ -1,21 +1,21 @@
 "use strict";
-let users = [
-  { name: "John", age: 20, surname: "Johnson" },
-  { name: "Pete", age: 18, surname: "Peterson" },
-  { name: "Ann", age: 19, surname: "Hathaway" }
-];
-// 이름을 기준으로 정렬(Ann, John, Pete)
-users.sort((a, b) => a.name > b.name ? 1 : -1);
+function makeArmy() {
+  let shooters = [];
 
-// 나이를 기준으로 정렬(Pete, Ann, John)
-users.sort((a, b) => a.age > b.age ? 1 : -1);
+  let i = 0;
+  while (i < 10) {
+    let shooter = function() { // shooter 함수
+      alert( i ); // 몇 번째 shooter인지 출력해줘야 함
+    };
+    shooters.push(shooter);
+    i++;
+  }
 
-function byField(fieldName){
-  return (a, b) => a[fieldName] > b[fieldName] ? 1 : -1;
+  return shooters;
 }
 
-console.log(users.sort(byField('name')))
-console.log(users)
-console.log(users.sort(byField('age')))
-console.log(users)
-alert(users);
+let army = makeArmy();
+
+army[0](); // 0번째 shooter가 10을 출력함
+army[5](); // 5번째 shooter 역시 10을 출력함
+// 모든 shooter가 자신의 번호 대신 10을 출력하고 있음
