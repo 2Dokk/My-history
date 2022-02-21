31a328a286f85eca375399badc3d626b60e38024
2doKK <2dokyoung@gmail.com>
2022-02-21 22:06:46 +0900

    20220221

--- test.js
+++ test.js
@@ -1,17 +1,21 @@
 "use strict";
-/* ... 여기에 두 함수 inBetween과 inArray을 만들어주세요 ...*/
-function inBetween(a,b){
-  return function(x){
-    return x >= a && x<= b;
-  }
-}
-function inArray(arr){
-  return function(x){
-    return arr.includes(x);
-  };
-}
-let arr = [1, 2, 3, 4, 5, 6, 7];
+let users = [
+  { name: "John", age: 20, surname: "Johnson" },
+  { name: "Pete", age: 18, surname: "Peterson" },
+  { name: "Ann", age: 19, surname: "Hathaway" }
+];
+// 이름을 기준으로 정렬(Ann, John, Pete)
+users.sort((a, b) => a.name > b.name ? 1 : -1);
 
-alert( arr.filter(inBetween(3, 6)) ); // 3,4,5,6
+// 나이를 기준으로 정렬(Pete, Ann, John)
+users.sort((a, b) => a.age > b.age ? 1 : -1);
 
-alert( arr.filter(inArray([1, 2, 10])) ); // 1,2
+function byField(fieldName){
+  return (a, b) => a[fieldName] > b[fieldName] ? 1 : -1;
+}
+
+console.log(users.sort(byField('name')))
+console.log(users)
+console.log(users.sort(byField('age')))
+console.log(users)
+alert(users);
