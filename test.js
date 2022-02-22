2f27fe7a24cf1a5b5bc8024084f30e0e229c8218
2doKK <2dokyoung@gmail.com>
2022-02-23 00:41:59 +0900

    20220223

--- test.js
+++ test.js
@@ -1,13 +1,12 @@
 "use strict";
-let start = Date.now();
-let times = [];
+function printNumber(from, to){
+  let current = from;
+  let timerID = setInterval(function(){
+    console.log(current);
+    if (current++ == to){
+      clearInterval(timerID);
+    }
+  }, 1000)
+}
 
-setTimeout(function run() {
-  times.push(Date.now() - start); // 이전 호출이 끝난 시점과 현재 호출이 시작된 시점의 시차를 기록
-
-  if (start + 100 < Date.now()) alert(times); // 지연 간격이 100ms를 넘어가면, array를 얼럿창에 띄워줌
-  else setTimeout(run); // 지연 간격이 100ms를 넘어가지 않으면 재스케줄링함
-});
-
-// 출력창 예시:
-// 1,1,1,1,9,15,20,24,30,35,40,45,50,55,59,64,70,75,80,85,90,95,100
+printNumber(5,10);
