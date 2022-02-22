5e1c9b4d547029704f7dc16628df3a601a804f7f
2doKK <2dokyoung@gmail.com>
2022-02-23 00:27:26 +0900

    20220223

--- test.js
+++ test.js
@@ -1,9 +1,13 @@
 "use strict";
-/** setInterval을 이용하지 않고 아래와 같이 중첩 setTimeout을 사용함
-let timerId = setInterval(() => alert('째깍'), 2000);
-*/
+let start = Date.now();
+let times = [];
 
-let timerId = setTimeout(function tick() {
-  alert('째깍');
-  timerId = setTimeout(tick, 2000); // (*)
-}, 2000);
+setTimeout(function run() {
+  times.push(Date.now() - start); // 이전 호출이 끝난 시점과 현재 호출이 시작된 시점의 시차를 기록
+
+  if (start + 100 < Date.now()) alert(times); // 지연 간격이 100ms를 넘어가면, array를 얼럿창에 띄워줌
+  else setTimeout(run); // 지연 간격이 100ms를 넘어가지 않으면 재스케줄링함
+});
+
+// 출력창 예시:
+// 1,1,1,1,9,15,20,24,30,35,40,45,50,55,59,64,70,75,80,85,90,95,100
