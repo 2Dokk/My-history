93697696a8e1f555bea9dc8fc605e2bf74e44344
2doKK <2dokyoung@gmail.com>
2022-02-24 23:09:12 +0900

    20220224

--- test.js
+++ test.js
@@ -1,32 +1,3 @@
 "use strict";
-// worker.slow에 캐싱 기능을 추가해봅시다.
-let worker = {
-  someMethod() {
-    return 1;
-  },
-
-  slow(x) {
-    // CPU 집약적인 작업이라 가정
-    alert(`slow(${x})을/를 호출함`);
-    return x * this.someMethod(); // (*)
-  }
-};
-
-// 이전과 동일한 코드
-function cachingDecorator(func) {
-  let cache = new Map();
-  return function(x) {
-    if (cache.has(x)) {
-      return cache.get(x);
-    }
-    let result = func(x); // (**)
-    cache.set(x, result);
-    return result;
-  };
-}
-
-alert( worker.slow(1) ); // 기존 메서드는 잘 동작합니다.
-
-worker.slow = cachingDecorator(worker.slow); // 캐싱 데코레이터 적용
-
-alert( worker.slow(2) ); // 에러 발생!, Error: Cannot read property 'someMethod' of undefined
+let func = worker.slow;
+func(2);
