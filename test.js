876bf62247c876ff74388e9614b0db849c317a8b
2doKK <2dokyoung@gmail.com>
2022-02-26 04:14:13 +0900

    20220226

--- test.js
+++ test.js
@@ -1,20 +1,9 @@
 "use strict";
-function delay(f, ms) {
-
-    return function(...args) {
-      setTimeout(function() {
-        f.apply(this, args); // use it here
-      }, ms);
-    };
+let user = {
+    firstName: "John",
+    sayHi() {
+      alert(`Hello, ${this.firstName}!`);
+    }
+  };
   
-}
-function f(x) {
-    alert(x);
-  }
-  
-  // create wrappers
-  let f1000 = delay(f, 1000);
-  let f1500 = delay(f, 1500);
-  
-  f1000("test"); // shows "test" after 1000ms
-  f1500("test"); // shows "test" after 1500ms
+  setTimeout(user.sayHi, 1000); // Hello, undefined!
