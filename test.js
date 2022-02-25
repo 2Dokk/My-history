a6a4654e5ef36681eb1b54ad03c01bdd1a5141f2
2doKK <2dokyoung@gmail.com>
2022-02-26 04:11:11 +0900

    20220226

--- test.js
+++ test.js
@@ -2,10 +2,19 @@
 function delay(f, ms) {
 
     return function(...args) {
-      let savedThis = this; // store this into an intermediate variable
       setTimeout(function() {
-        f.apply(savedThis, args); // use it here
+        f.apply(this, args); // use it here
       }, ms);
     };
   
+}
+function f(x) {
+    alert(x);
   }
+  
+  // create wrappers
+  let f1000 = delay(f, 1000);
+  let f1500 = delay(f, 1500);
+  
+  f1000("test"); // shows "test" after 1000ms
+  f1500("test"); // shows "test" after 1500ms
