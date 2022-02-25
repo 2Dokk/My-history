8f26efd7996119d753ba52b4d85a61cfa78b0c66
2doKK <2dokyoung@gmail.com>
2022-02-26 03:56:46 +0900

    20220226

--- test.js
+++ test.js
@@ -1,30 +1,17 @@
 "use strict";
-function work(a, b) {
-    alert( a + b ); // work is an arbitrary function or method
+function f(x) {
+    alert(x);
   }
+  // create wrappers
+  let f1000 = delay(f, 1000);
+  let f1500 = delay(f, 1500);
   
-  work = spy(work);
+  f1000("test"); // shows "test" after 1000ms
+  f1500("test"); // shows "test" after 1500ms
   
-  work(1, 2); // 3
-  work(4, 5); // 9
-  
-  for (let args of work.calls) {
-    alert( 'call:' + args.join() ); // "call:1,2", "call:4,5"
-  }
+  function delay(f,ms){
+      let timerId = setTimeout(f,ms);
 
-function spy(func){
-    let cache = new Map();
-    function hash(args){
-        return args[0]+','+args[1];
-    }
-    return function(x){
-        let key = hash(arguments);
-        if (cache.has(key)){
-            return cache.get(key);
-        }
-        let result = func.call(this,...arguments);
-        cache.set(key,result);
-        return result;
-
-    };
+    
+      return f;
 }
