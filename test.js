e6b606cc4bf3df649ce6dcd32a5d90cddd280115
2doKK <2dokyoung@gmail.com>
2022-02-26 04:00:26 +0900

    20220226

--- test.js
+++ test.js
@@ -1,16 +1,13 @@
 "use strict";
-function f(x) {
-    alert(x);
+function delay(f, ms) {
+
+    return function() {
+      setTimeout(() => f.apply(this, arguments), ms);
+    };
+  
   }
-  // create wrappers
-  let f1000 = delay(f, 1000);
-  let f1500 = delay(f, 1500);
+  
+  let f1000 = delay(alert, 1000);
   
   f1000("test"); // shows "test" after 1000ms
-  f1500("test"); // shows "test" after 1500ms
   
-  function delay(f,ms){
-      let timerId = setTimeout(f,ms);
-
-      return f;
-    }
