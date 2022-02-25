8da817ca9ecd23ce783136366af78d180b58d2b2
2doKK <2dokyoung@gmail.com>
2022-02-25 18:59:11 +0900

    20220225

--- test.js
+++ test.js
@@ -1,11 +1,28 @@
 "use strict";
-function sayHi() {
-    alert(this.name);
+let worker = {
+    someMethod() {
+      return 1;
+    },
+  
+    slow(x) {
+      alert(`slow(${x})을/를 호출함`);
+      return x * this.someMethod(); // (*)
+    }
+  };
+  
+  function cachingDecorator(func) {
+    let cache = new Map();
+    return function(x) {
+      if (cache.has(x)) {
+        return cache.get(x);
+      }
+      let result = func.call(this, x); // 이젠 'this'가 제대로 전달됩니다.
+      cache.set(x, result);
+      return result;
+    };
   }
   
-  let user = { name: "John" };
-  let admin = { name: "Admin" };
+  worker.slow = cachingDecorator(worker.slow); // 캐싱 데코레이터 적용
   
-  // call을 사용해 원하는 객체가 'this'가 되도록 합니다.
-  sayHi.call( user ); // this = John
-  sayHi.call( admin ); // this = Admin
+  alert( worker.slow(2) ); // 제대로 동작합니다.
+  alert( worker.slow(2) ); // 제대로 동작합니다. 다만, 원본 함수가 호출되지 않고 캐시 된 값이 출력됩니다.
