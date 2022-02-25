dc43d323a58fa8679bf149e2aec09a249ba6aca8
2doKK <2dokyoung@gmail.com>
2022-02-25 18:56:45 +0900

    20220225

--- test.js
+++ test.js
@@ -1,3 +1,11 @@
 "use strict";
-let func = worker.slow;
-func(2);
+function sayHi() {
+    alert(this.name);
+  }
+  
+  let user = { name: "John" };
+  let admin = { name: "Admin" };
+  
+  // call을 사용해 원하는 객체가 'this'가 되도록 합니다.
+  sayHi.call( user ); // this = John
+  sayHi.call( admin ); // this = Admin
