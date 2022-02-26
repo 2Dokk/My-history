30375c8715cac1eaa8680ef86740c8caf9d64246
2doKK <2dokyoung@gmail.com>
2022-02-26 22:43:57 +0900

    20220226

--- test.js
+++ test.js
@@ -1,13 +1,18 @@
 "use strict";
-let group = {
-    title: "1모둠",
-    students: ["보라", "호진", "지민"],
+function User(name, birthday) {
+    this.name = name;
+    this.birthday = birthday;
   
-    showList() {
-      this.students.forEach(
-        student => alert(this.title + ': ' + student)
-      );
-    }
-  };
+    // age는 현재 날짜와 생일을 기준으로 계산됩니다.
+    Object.defineProperty(this, "age", {
+      get() {
+        let todayYear = new Date().getFullYear();
+        return todayYear - this.birthday.getFullYear();
+      }
+    });
+  }
   
-  group.showList();
+  let john = new User("John", new Date(1992, 6, 1));
+  
+  alert( john.birthday ); // birthday를 사용할 수 있습니다.
+  alert( john.age );      // age 역시 사용할 수 있습니다.
