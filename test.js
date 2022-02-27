d14afc942adf0b799910a140a40d00cce5326d38
2doKK <2dokyoung@gmail.com>
2022-02-27 23:41:18 +0900

    20220227

--- test.js
+++ test.js
@@ -1,9 +1,15 @@
 "use strict";
 let animal = {
-    eats: true
-  };
-  let rabbit = {
-    jumps: true
+    eats: true,
+    walk() {
+      alert("동물이 걷습니다.");
+    }
   };
   
-  rabbit.__proto__ = animal;
+  let rabbit = {
+    jumps: true,
+    __proto__: animal
+  };
+  
+  // 메서드 walk는 rabbit의 프로토타입인 animal에서 상속받았습니다.
+  rabbit.walk(); // 동물이 걷습니다.adsf
