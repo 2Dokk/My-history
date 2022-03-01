d4829b44dbba53d4b2edde53f9eae49a272cfd10
2doKK <2dokyoung@gmail.com>
2022-03-02 00:47:43 +0900

    20220302

--- test.js
+++ test.js
@@ -1,2 +1,19 @@
 "use strict";
-let clone = Object.create(Object.getPrototypeOf(obj), Object.getOwnPropertyDescriptors(obj));
+let dictionary = Object.create(null, {
+    toString: { // toString 프로퍼티를 정의합니다.
+      value() { // value는 함수입니다.
+        return Object.keys(this).join();
+      }
+    }
+});
+  
+dictionary.apple = "Apple";
+dictionary.__proto__ = "test";
+  
+// apple과 __proto__는 반복문 안에 있습니다.
+for(let key in dictionary) {
+  alert(key); // "apple" 다음 "__proto__"가 있습니다.
+}
+  
+// toString에 의해 쉼표로 구분된 프로퍼티 목록
+alert(dictionary); // "apple,__proto__"
