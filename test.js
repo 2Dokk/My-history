96aa6cd240661bd38a04ebfaf84163f72dc202e5
2doKK <2dokyoung@gmail.com>
2022-03-01 20:16:42 +0900

    20220301

--- test.js
+++ test.js
@@ -1,14 +1,1 @@
 "use strict";
-Function.prototype.defer = function(ms) {
-    let f = this;
-    return function(...args) {
-      setTimeout(() => f.apply(this, args), ms);
-    }
-  };
-  
-  // 확인해 보세요.
-  function f(a, b) {
-    alert( a + b );
-  }
-  
-  f.defer(1000)(1, 2); // 1초 후 3 출력
