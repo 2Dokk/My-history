ce8346cff9c10865d0d13283bad1631e6d6a947f
2doKK <2dokyoung@gmail.com>
2022-03-02 00:51:53 +0900

    20220302

--- test.js
+++ test.js
@@ -1,19 +1,14 @@
 "use strict";
-let dictionary = Object.create(null, {
-    toString: { // toString 프로퍼티를 정의합니다.
-      value() { // value는 함수입니다.
-        return Object.keys(this).join();
-      }
-    }
-});
+function Rabbit(name) {
+    this.name = name;
+  }
+Rabbit.prototype.sayHi = function() {
+    alert(this.name);
+  };
   
-dictionary.apple = "Apple";
-dictionary.__proto__ = "test";
-  
-// apple과 __proto__는 반복문 안에 있습니다.
-for(let key in dictionary) {
-  alert(key); // "apple" 다음 "__proto__"가 있습니다.
-}
-  
-// toString에 의해 쉼표로 구분된 프로퍼티 목록
-alert(dictionary); // "apple,__proto__"
+let rabbit = new Rabbit("Rabbit");
+
+rabbit.sayHi();
+Rabbit.prototype.sayHi();
+Object.getPrototypeOf(rabbit).sayHi();
+rabbit.__proto__.sayHi();
