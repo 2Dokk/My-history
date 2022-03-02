a7a7684490f20bb64934bec2d2d1ee3426bb3096
2doKK <2dokyoung@gmail.com>
2022-03-02 17:26:44 +0900

    20220302

--- test.js
+++ test.js
@@ -1,16 +1,17 @@
 "use strict";
 class User {
-
-    constructor(name) {
-      this.name = name;
-    }
-  
-    sayHi() {
-      alert(this.name);
-    }
-  
+    constructor(name) { this.name = name; }
+    sayHi() { alert(this.name); }
   }
   
-  // 사용법:
-  let user = new User("John");
-  user.sayHi();
+  // 클래스는 함수입니다.
+  alert(typeof User); // function
+  
+  // 정확히는 생성자 메서드와 동일합니다.
+  alert(User === User.prototype.constructor); // true
+  
+  // 클래스 내부에서 정의한 메서드는 User.prototype에 저장됩니다.
+  alert(User.prototype.sayHi); // alert(this.name);
+  
+  // 현재 프로토타입에는 메서드가 두 개입니다.
+  alert(Object.getOwnPropertyNames(User.prototype)); // constructor, sayHi
