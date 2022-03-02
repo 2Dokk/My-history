9f5c6fbff0cd37e42ef4777ef3cb2686ed967c5f
2doKK <2dokyoung@gmail.com>
2022-03-03 08:42:04 +0900

    20220303

--- test.js
+++ test.js
@@ -1,40 +1,17 @@
 "use strict";
-
-
-
-class Clock{
-  constructor(template){
-    this.template = template;
+class Animal {
+  constructor(name) {
+    this.speed = 0;
+    this.name = name;
   }
-  stop = () => {
-    clearInterval(timer);
+  run(speed) {
+    this.speed = speed;
+    alert(`${this.name} 은/는 속도 ${this.speed}로 달립니다.`);
   }
-  
-  start = () => {
-    this.render();
-    var timer = setInterval(this.render, 1000);
-  }
-
-  render = () => {
-    let date = new Date();
-
-    let hours = date.getHours();
-    if (hours < 10) hours = '0' + hours;
-
-    let mins = date.getMinutes();
-    if (mins < 10) mins = '0' + mins;
-
-    let secs = date.getSeconds();
-    if (secs < 10) secs = '0' + secs;
-
-    let output = this.template
-      .replace('h', hours)
-      .replace('m', mins)
-      .replace('s', secs);
-
-    console.log(output);
+  stop() {
+    this.speed = 0;
+    alert(`${this.name} 이/가 멈췄습니다.`);
   }
 }
 
-let clock = new Clock('h:m:s');
-clock.start();
+let animal = new Animal("동물");
