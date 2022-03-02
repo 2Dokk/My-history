a92edf40649fa9cd4b5196c49b5735731e203fb2
2doKK <2dokyoung@gmail.com>
2022-03-03 00:22:49 +0900

    20220303

--- test.js
+++ test.js
@@ -1,4 +1,14 @@
 "use strict";
-class User{
-  constructor() {}
+function makeClass(phrase) {
+  // 클래스를 선언하고 이를 반환함
+  return class {
+    sayHi() {
+      alert(phrase);
+    };
+  };
 }
+
+// 새로운 클래스를 만듦
+let User = makeClass("안녕하세요.");
+
+new User().sayHi(); // 안녕하세요.
