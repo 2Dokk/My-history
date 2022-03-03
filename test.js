0be77fa4c5cdafe2218d3311ae51ea60a39f3bac
2doKK <2dokyoung@gmail.com>
2022-03-04 00:46:12 +0900

    20220304

--- test.js
+++ test.js
@@ -1,37 +1,11 @@
 "use strict";
-class Animal {
-  static planet = "지구";
-
-  constructor(name, speed) {
-    this.speed = speed;
+class Rabbit {
+  constructor(name) {
     this.name = name;
   }
-
-  run(speed = 0) {
-    this.speed += speed;
-    alert(`${this.name}가 속도 ${this.speed}로 달립니다.`);
-  }
-
-  static compare(animalA, animalB) {
-    return animalA.speed - animalB.speed;
-  }
-
 }
 
-// Animal을 상속받음
-class Rabbit extends Animal {
-  hide() {
-    alert(`${this.name}가 숨었습니다!`);
-  }
-}
+let rabbit = new Rabbit("Rab");
 
-let rabbits = [
-  new Rabbit("흰 토끼", 10),
-  new Rabbit("검은 토끼", 5)
-];
-
-rabbits.sort(Rabbit.compare);
-
-rabbits[0].run(); // 검은 토끼가 속도 5로 달립니다.
-
-alert(Rabbit.planet); // 지구
+// 메서드 hasOwnProperty는 Object.prototype에서 왔습니다.
+alert( rabbit.hasOwnProperty('name') ); // true
