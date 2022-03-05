ba3b8729968b72ebc4d4a916be0027e4e73ad2f1
2doKK <2dokyoung@gmail.com>
2022-03-05 23:34:03 +0900

    20220305

--- test.js
+++ test.js
@@ -1,23 +1,41 @@
 "use strict";
-// 믹스인
-let sayHiMixin = {
-  sayHi() {
-    alert(`Hello ${this.name}`);
+let eventMixin = {
+  /**
+   *  이벤트 구독
+   *  사용패턴: menu.on('select', function(item) { ... }
+  */
+  on(eventName, handler) {
+    if (!this._eventHandlers) this._eventHandlers = {};
+    if (!this._eventHandlers[eventName]) {
+      this._eventHandlers[eventName] = [];
+    }
+    this._eventHandlers[eventName].push(handler);
   },
-  sayBye() {
-    alert(`Bye ${this.name}`);
+
+  /**
+   *  구독 취소
+   *  사용패턴: menu.off('select', handler)
+   */
+  off(eventName, handler) {
+    let handlers = this._eventHandlers?.[eventName];
+    if (!handlers) return;
+    for (let i = 0; i < handlers.length; i++) {
+      if (handlers[i] === handler) {
+        handlers.splice(i--, 1);
+      }
+    }
+  },
+
+  /**
+   *  주어진 이름과 데이터를 기반으로 이벤트 생성
+   *  사용패턴: this.trigger('select', data1, data2);
+   */
+  trigger(eventName, ...args) {
+    if (!this._eventHandlers?.[eventName]) {
+      return; // no handlers for that event name
+    }
+
+    // 핸들러 호출
+    this._eventHandlers[eventName].forEach(handler => handler.apply(this, args));
   }
 };
-
-// 사용법:
-class User {
-  constructor(name) {
-    this.name = name;
-  }
-}
-
-// 메서드 복사
-Object.assign(User.prototype, sayHiMixin);
-
-// 이제 User가 인사를 할 수 있습니다.
-new User("Dude").sayHi(); // Hello Dude!
