77d8b68652b9ffaca25827730a3129174c09c8e1
2doKK <2dokyoung@gmail.com>
2022-03-05 23:17:59 +0900

    20220305

--- test.js
+++ test.js
@@ -1,12 +1,9 @@
 "use strict";
-// canEat 프로퍼티가 있으면 animal이라고 판단할 수 있도록
-// instanceOf의 로직을 직접 설정합니다.
-class Animal {
-  static [Symbol.hasInstance](obj) {
-    if (obj.canEat) return true;
-  }
-}
+class Animal {}
+class Rabbit extends Animal {}
 
-let obj = { canEat: true };
+let rabbit = new Rabbit();
+alert(rabbit instanceof Animal); // true
 
-alert(obj instanceof Animal); // true, Animal[Symbol.hasInstance](obj)가 호출됨
+// rabbit.__proto__ === Rabbit.prototype
+// rabbit.__proto__.__proto__ === Animal.prototype (일치!)
