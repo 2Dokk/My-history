fad8e136376cbfbc0dcb420d49476d987dc454f9
2doKK <2dokyoung@gmail.com>
2022-03-06 21:44:50 +0900

    20220306

--- test.js
+++ test.js
@@ -1,8 +1,17 @@
 "use strict";
+
 class ValidationError extends Error {
     constructor(message) {
       super(message);
       this.name = "ValidationError";
+    }
+  }
+  
+  class PropertyRequiredError extends ValidationError {
+    constructor(property) {
+      super("No property: " + property);
+      this.name = "PropertyRequiredError";
+      this.property = property;
     }
   }
   
@@ -11,25 +20,27 @@ class ValidationError extends Error {
     let user = JSON.parse(json);
   
     if (!user.age) {
-      throw new ValidationError("No field: age");
+      throw new PropertyRequiredError("age");
     }
     if (!user.name) {
-      throw new ValidationError("No field: name");
+      throw new PropertyRequiredError("name");
     }
   
     return user;
   }
   
-  // try..catch와 readUser를 함께 사용한 예시
+  // try..catch와 readUser를 함께 사용하면 다음과 같습니다.
   
   try {
     let user = readUser('{ "age": 25 }');
   } catch (err) {
     if (err instanceof ValidationError) {
-      alert("Invalid data: " + err.message); // Invalid data: No field: name
-    } else if (err instanceof SyntaxError) { // (*)
+      alert("Invalid data: " + err.message); // Invalid data: No property: name
+      alert(err.name); // PropertyRequiredError
+      alert(err.property); // name
+    } else if (err instanceof SyntaxError) {
       alert("JSON Syntax Error: " + err.message);
     } else {
-      throw err; // 알려지지 않은 에러는 재던지기 합니다. (**)
+      throw err; // 알려지지 않은 에러는 재던지기 합니다.
     }
   }
