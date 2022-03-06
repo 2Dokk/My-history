6fba344746df92f7e4b0b13d8772775e1cb0a6ae
2doKK <2dokyoung@gmail.com>
2022-03-06 18:15:46 +0900

    20220306

--- test.js
+++ test.js
@@ -1,2 +1,16 @@
 "use strict";
-let error = new Error(message);
+let json = '{ "age": 30 }'; // 불완전한 데이터
+
+try {
+
+  let user = JSON.parse(json); // <-- 에러 없음
+
+  if (!user.name) {
+    throw new SyntaxError("불완전한 데이터: 이름 없음"); // (*)
+  }
+
+  alert( user.name );
+
+} catch(e) {
+  alert( "JSON Error: " + e.message ); // JSON Error: 불완전한 데이터: 이름 없음
+}
