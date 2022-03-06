191290098a79c18da50310c47562d29373aeabf9
2doKK <2dokyoung@gmail.com>
2022-03-06 21:34:33 +0900

    20220306

--- test.js
+++ test.js
@@ -1,25 +1,35 @@
 "use strict";
-let num = +prompt("양의 정수를 입력해주세요.", 35)
-
-let diff, result;
-
-function fib(n) {
-  if (n < 0 || Math.trunc(n) != n) {
-    throw new Error("음수나 정수가 아닌 값은 처리할 수 없습니다.");
+class ValidationError extends Error {
+    constructor(message) {
+      super(message);
+      this.name = "ValidationError";
+    }
   }
-  return n <= 1 ? n : fib(n - 1) + fib(n - 2);
-}
-
-let start = Date.now();
-
-try {
-  result = fib(num);
-} catch (e) {
-  result = 0;
-} finally {
-  diff = Date.now() - start;
-}
-
-alert(result || "에러 발생");
-
-alert( `연산 시간: ${diff}ms` );
+  
+  // 사용법
+  function readUser(json) {
+    let user = JSON.parse(json);
+  
+    if (!user.age) {
+      throw new ValidationError("No field: age");
+    }
+    if (!user.name) {
+      throw new ValidationError("No field: name");
+    }
+  
+    return user;
+  }
+  
+  // try..catch와 readUser를 함께 사용한 예시
+  
+  try {
+    let user = readUser('{ "age": 25 }');
+  } catch (err) {
+    if (err instanceof ValidationError) {
+      alert("Invalid data: " + err.message); // Invalid data: No field: name
+    } else if (err instanceof SyntaxError) { // (*)
+      alert("JSON Syntax Error: " + err.message);
+    } else {
+      throw err; // 알려지지 않은 에러는 재던지기 합니다. (**)
+    }
+  }
