2112e22a7678661e7eba7c0472d2a3a99f4bea69
2doKK <2dokyoung@gmail.com>
2022-03-06 18:10:16 +0900

    20220306

--- test.js
+++ test.js
@@ -1,8 +1,2 @@
 "use strict";
-let json = '{"name":"John", "age": 30}'; // 서버로부터 전달받은 데이터
-
-let user = JSON.parse(json); // 전달받은 문자열을 자바스크립트 객체로 변환
-
-// 문자열 형태로 전달받은 user가 프로퍼티를 가진 객체가 됨
-alert( user.name ); // John
-alert( user.age );  // 30
+let error = new Error(message);
