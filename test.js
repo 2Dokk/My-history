92e7cef711d8dbee5bb03019b134c10c9a06f1f9
2doKK <2dokyoung@gmail.com>
2022-03-08 00:35:28 +0900

    20220308

--- test.js
+++ test.js
@@ -1,24 +1,10 @@
 "use strict";
-// 실행 순서: catch -> catch
-new Promise((resolve, reject) => {
-
-  throw new Error("에러 발생!");
-
-}).catch(function(error) { // (*)
-
-  if (error instanceof URIError) {
-    // 에러 처리
-  } else {
-    alert("처리할 수 없는 에러");
-
-    throw error; // 에러 다시 던지기
-  }
-
-}).then(function() {
-  /* 여기는 실행되지 않습니다. */
-}).catch(error => { // (**)
-
-  alert(`알 수 없는 에러가 발생함: ${error}`);
-  // 반환값이 없음 => 실행이 계속됨
-
+window.addEventListener('unhandledrejection', function(event) {
+  // 이벤트엔 두 개의 특별 프로퍼티가 있습니다.
+  alert(event.promise); // [object Promise] - 에러를 생성하는 프라미스
+  alert(event.reason); // Error: 에러 발생! - 처리하지 못한 에러 객체
 });
+
+new Promise(function() {
+  throw new Error("에러 발생!");
+}); // 에러 처리 핸들러, catch가 없음
