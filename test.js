a16d6781f13bcb9acb1ae23bf8308a6b6c4409c4
2doKK <2dokyoung@gmail.com>
2022-03-08 00:31:43 +0900

    20220308

--- test.js
+++ test.js
@@ -1,6 +1,24 @@
 "use strict";
+// 실행 순서: catch -> catch
 new Promise((resolve, reject) => {
-  resolve("ok");
-}).then((result) => {
-  throw new Error("에러 발생!"); // 프라미스가 거부됨
-}).catch(alert); // Error: 에러 발생!
+
+  throw new Error("에러 발생!");
+
+}).catch(function(error) { // (*)
+
+  if (error instanceof URIError) {
+    // 에러 처리
+  } else {
+    alert("처리할 수 없는 에러");
+
+    throw error; // 에러 다시 던지기
+  }
+
+}).then(function() {
+  /* 여기는 실행되지 않습니다. */
+}).catch(error => { // (**)
+
+  alert(`알 수 없는 에러가 발생함: ${error}`);
+  // 반환값이 없음 => 실행이 계속됨
+
+});
