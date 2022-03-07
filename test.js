6c4bdcbec12b8c8c3cefa87f170966fa72266310
2doKK <2dokyoung@gmail.com>
2022-03-07 23:55:07 +0900

    20220307

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
 "use strict";
-function loadScript(src, callback) {
-  let script = document.createElement('script');
-  script.src = src;
+function loadScript(src) {
+  return new Promise(function(resolve, reject) {
+    let script = document.createElement('script');
+    script.src = src;
 
-  script.onload = () => callback(null, script);
-  script.onerror = () => callback(new Error(`${src}를 불러오는 도중에 에러가 발생함`));
+    script.onload = () => resolve(script);
+    script.onerror = () => reject(new Error(`${src}를 불러오는 도중에 에러가 발생함`));
 
-  document.head.append(script);
+    document.head.append(script);
+  });
 }
