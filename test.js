d8560ea2d6d67e80eac84c25233125e9638efcc9
2doKK <2dokyoung@gmail.com>
2022-03-08 00:06:19 +0900

    20220307

--- test.js
+++ test.js
@@ -1,21 +1,11 @@
 "use strict";
-function loadScript(src) {
-  return new Promise(function(resolve, reject) {
-    let script = document.createElement('script');
-    script.src = src;
-
-    script.onload = () => resolve(script);
-    script.onerror = () => reject(new Error(`${src}를 불러오는 도중에 에러가 발생함`));
-
-    document.head.append(script);
-  });
+function delay(ms) {
+  return new Promise(function(resolve,reject){
+    setTimeout(() => {
+      resolve()
+    }, ms);
+  }
+  )
 }
 
-let promise = loadScript("https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.js");
-
-promise.then(
-  script => alert(`${script.src}을 불러왔습니다!`),
-  error => alert(`Error: ${error.message}`)
-);
-
-promise.then(script => alert('또다른 핸들러...'));
+delay(3000).then(() => alert('3초후 실행'));
