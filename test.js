6d9126998f4246432e722de682de0d5c64e00aca
2doKK <2dokyoung@gmail.com>
2022-03-07 23:04:59 +0900

    20220307

--- test.js
+++ test.js
@@ -1,28 +1,4 @@
 "use strict";
-loadScript('1.js', step1);
-
-function step1(error, script) {
-  if (error) {
-    handleError(error);
-  } else {
-    // ...
-    loadScript('2.js', step2);
-  }
-}
-
-function step2(error, script) {
-  if (error) {
-    handleError(error);
-  } else {
-    // ...
-    loadScript('3.js', step3);
-  }
-}
-
-function step3(error, script) {
-  if (error) {
-    handleError(error);
-  } else {
-    // 모든 스크립트가 로딩되면 다른 동작을 수행합니다. (*)
-  }
-};
+let promise = new Promise(function(resolve, reject) {
+  // executor (제작 코드, '가수')
+});
