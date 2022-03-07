c7191b812e787717c6592497d5d872bb440c0096
2doKK <2dokyoung@gmail.com>
2022-03-07 23:14:43 +0900

    20220307

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 "use strict";
 let promise = new Promise(function(resolve, reject) {
-  setTimeout(() => resolve("done!"), 1000);
+  setTimeout(() => reject(new Error("에러 발생!")), 1000);
 });
 
-// resolve 함수는 .then의 첫 번째 함수(인수)를 실행합니다.
+// reject 함수는 .then의 두 번째 함수를 실행합니다.
 promise.then(
-  result => alert(result), // 1초 후 "done!"을 출력
-  error => alert(error) // 실행되지 않음
+  result => alert(result), // 실행되지 않음
+  error => alert(error) // 1초 후 "Error: 에러 발생!"를 출력
 );
