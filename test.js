8f8000322a26fceb63a04275bc4543a840d46705
2doKK <2dokyoung@gmail.com>
2022-03-08 01:25:37 +0900

    20220308

--- test.js
+++ test.js
@@ -2,14 +2,17 @@
 let urls = [
   'https://api.github.com/users/iliakan',
   'https://api.github.com/users/remy',
-  'https://api.github.com/users/jeresig'
+  'https://no-such-url'
 ];
 
-// fetch를 사용해 url을 프라미스로 매핑합니다.
-let requests = urls.map(url => fetch(url));
-
-// Promise.all은 모든 작업이 이행될 때까지 기다립니다.
-Promise.all(requests)
-  .then(responses => responses.forEach(
-    response => alert(`${response.url}: ${response.status}`)
-  ));
+Promise.allSettled(urls.map(url => fetch(url)))
+  .then(results => { // (*)
+    results.forEach((result, num) => {
+      if (result.status == "fulfilled") {
+        alert(`${urls[num]}: ${result.value.status}`);
+      }
+      if (result.status == "rejected") {
+        alert(`${urls[num]}: ${result.reason}`);
+      }
+    });
+  });
