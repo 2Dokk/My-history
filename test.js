7e459bd4cc7c0507c233121591c29c8ebbd41150
2doKK <2dokyoung@gmail.com>
2022-03-08 00:21:39 +0900

    20220308

--- test.js
+++ test.js
@@ -1,17 +1,6 @@
 "use strict";
-fetch('/article/promise-chaining/user.json')
-  .then(response => response.json())
-  .then(user => fetch(`https://api.github.com/users/${user.name}`))
-  .then(response => response.json())
-  .then(githubUser => new Promise((resolve, reject) => {
-    let img = document.createElement('img');
-    img.src = githubUser.avatar_url;
-    img.className = "promise-avatar-example";
-    document.body.append(img);
-
-    setTimeout(() => {
-      img.remove();
-      resolve(githubUser);
-    }, 3000);
-  }))
-  .catch(error => alert(error.message));
+new Promise((resolve, reject) => {
+  resolve("ok");
+}).then((result) => {
+  throw new Error("에러 발생!"); // 프라미스가 거부됨
+}).catch(alert); // Error: 에러 발생!
