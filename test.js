03080f329fe1df571a376a01bf320669e7e3bd26
2doKK <2dokyoung@gmail.com>
2022-03-11 00:09:42 +0900

    20220310

--- test.js
+++ test.js
@@ -1,14 +1,30 @@
 "use strict";
-async function loadJson(url) { // (1)
-  let response = await fetch(url); // (2)
-
-  if (response.status == 200) {
-    let json = await response.json(); // (3)
-    return json;
+class HttpError extends Error {
+  constructor(response) {
+    super(`${response.status} for ${response.url}`);
+    this.name = 'HttpError';
+    this.response = response;
   }
-
-  throw new Error(response.status);
 }
 
-loadJson('no-such-user.json')
-  .catch(alert); // Error: 404 (4)
+async function loadJson(url){
+  let response = await fetch(url);
+  if (response.status == 200){
+    let json = await response.json();
+    return json;
+  } else {
+    throw new HttpError(response);
+  }
+}
+
+async function demoGithubUser() {
+  do {let name = prompt("GitHub username을 입력하세요.", "iliakan");
+    var userJson = await loadJson(`https://api.github.com/users/${name}`);
+    if (userJson instanceof HttpError && userJson.response.status == 400){
+      alert("일치하는 사용자가 없습니다. 다시 입력해 주세요.");
+    }
+  } while (userJson.name);
+  alert(`이름: ${userJson.name}.`);
+}
+
+demoGithubUser();
