21b6ea95d79d1eaf8162f5b7e7856435fae8e466
2doKK <2dokyoung@gmail.com>
2022-03-10 23:22:58 +0900

    20220310

--- test.js
+++ test.js
@@ -1,6 +1,13 @@
 "use strict";
 async function f() {
-  return 1;
+
+  let promise = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("완료!"), 1000)
+  });
+
+  let result = await promise; // 프라미스가 이행될 때까지 기다림 (*)
+
+  alert(result); // "완료!"
 }
 
-f().then(alert); // 1
+f();
