dd67769d2a0eb29a6663b5e7e3b386ca126f6318
2doKK <2dokyoung@gmail.com>
2022-03-10 23:03:12 +0900

    20220310

--- test.js
+++ test.js
@@ -1,24 +1,4 @@
 "use strict";
-// 콜백의 성공 결과를 담은 배열을 얻게 해주는 promisify(f, true)
-function promisify(f, manyArgs = false) {
-  return function (...args) {
-    return new Promise((resolve, reject) => {
-      function callback(err, ...results) { // f에 사용할 커스텀 콜백
-        if (err) {
-          reject(err);
-        } else {
-          // manyArgs가 구체적으로 명시되었다면, 콜백의 성공 케이스와 함께 이행 상태가 됩니다.
-          resolve(manyArgs ? results : results[0]);
-        }
-      }
-
-      args.push(callback);
-
-      f.call(this, ...args);
-    });
-  };
-};
-
-// 사용법:
-f = promisify(f, true);
-f(...).then(arrayOfResults => ..., err => ...)
+Promise.resolve()
+  .then(() => alert("프라미스 성공!"))
+  .then(() => alert("코드 종료"));
