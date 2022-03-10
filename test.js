365226fccd8b76bb734b9d5dff6138b2c3653d6d
2doKK <2dokyoung@gmail.com>
2022-03-10 23:49:17 +0900

    20220310

--- test.js
+++ test.js
@@ -1,10 +1,14 @@
 "use strict";
-class Waiter {
-  async wait() {
-    return await Promise.resolve(1);
+async function loadJson(url) { // (1)
+  let response = await fetch(url); // (2)
+
+  if (response.status == 200) {
+    let json = await response.json(); // (3)
+    return json;
   }
+
+  throw new Error(response.status);
 }
 
-new Waiter()
-  .wait()
-  .then(alert); // 1
+loadJson('no-such-user.json')
+  .catch(alert); // Error: 404 (4)
