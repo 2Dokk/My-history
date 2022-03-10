8e7435b18990f99ae4d49093aee75bd37d13d84d
2doKK <2dokyoung@gmail.com>
2022-03-10 23:25:49 +0900

    20220310

--- test.js
+++ test.js
@@ -1,13 +1,26 @@
 "use strict";
-async function f() {
+async function showAvatar() {
 
-  let promise = new Promise((resolve, reject) => {
-    setTimeout(() => resolve("완료!"), 1000)
-  });
+  // JSON 읽기
+  let response = await fetch('/article/promise-chaining/user.json');
+  let user = await response.json();
 
-  let result = await promise; // 프라미스가 이행될 때까지 기다림 (*)
+  // github 사용자 정보 읽기
+  let githubResponse = await fetch(`https://api.github.com/users/${user.name}`);
+  let githubUser = await githubResponse.json();
 
-  alert(result); // "완료!"
+  // 아바타 보여주기
+  let img = document.createElement('img');
+  img.src = githubUser.avatar_url;
+  img.className = "promise-avatar-example";
+  document.body.append(img);
+
+  // 3초 대기
+  await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+
+  img.remove();
+
+  return githubUser;
 }
 
-f();
+showAvatar();
