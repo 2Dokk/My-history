a98910df065582408007f077f1943abf740c5d53
2doKK <2dokyoung@gmail.com>
2022-03-10 22:42:14 +0900

    20220310

--- test.js
+++ test.js
@@ -1,12 +1,22 @@
 "use strict";
-let loadScriptPromise = function(src) {
-  return new Promise((resolve, reject) => {
-    loadScript(src, (err, script) => {
-      if (err) reject(err)
-      else resolve(script);
+function promisify(f) {
+  return function (...args) { // 래퍼 함수를 반환함
+    return new Promise((resolve, reject) => {
+      function callback(err, result) { // f에 사용할 커스텀 콜백
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      }
+
+      args.push(callback); // 위에서 만든 커스텀 콜백을 함수 f의 인수 끝에 추가합니다.
+
+      f.call(this, ...args); // 기존 함수를 호출합니다.
     });
-  })
-}
+  };
+};
 
 // 사용법:
-// loadScriptPromise('path/script.js').then(...)
+let loadScriptPromise = promisify(loadScript);
+loadScriptPromise(...).then(...);
