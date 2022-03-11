86d4534b8d38c092a1a8de803c65aad40432b376
2doKK <2dokyoung@gmail.com>
2022-03-12 00:25:46 +0900

    20220312

--- test.js
+++ test.js
@@ -1,36 +1,19 @@
 "use strict";
-async function* fetchCommits(repo) {
-  let url = `https://api.github.com/repos/${repo}/commits`;
+let user = { };
 
-  while (url) {
-    const response = await fetch(url, { // (1)
-      headers: {'User-Agent': 'Our script'}, // GitHub는 모든 요청에 user-agent헤더를 강제 합니다.
-    });
+user = new Proxy(user, {
+  ownKeys(target) { // 프로퍼티 리스트를 얻을 때 딱 한 번 호출됩니다.
+    return ['a', 'b', 'c'];
+  },
 
-    const body = await response.json(); // (2) 응답은 JSON 형태로 옵니다(커밋이 담긴 배열).
-
-    // (3) 헤더에 담긴 다음 페이지를 나타내는 URL을 추출합니다.
-    let nextPage = response.headers.get('Link').match(/<(.*?)>; rel="next"/);
-    nextPage = nextPage?.[1];
-
-    url = nextPage;
-
-    for(let commit of body) { // (4) 페이지가 끝날 때까지 커밋을 하나씩 반환(yield)합니다.
-      yield commit;
-    }
-  }
-}
-(async () => {
-
-  let count = 0;
-
-  for await (const commit of fetchCommits('javascript-tutorial/en.javascript.info')) {
-
-    console.log(commit.author.login);
-
-    if (++count == 100) { // 100번째 커밋에서 멈춥니다.
-      break;
-    }
+  getOwnPropertyDescriptor(target, prop) { // 모든 프로퍼티를 대상으로 호출됩니다.
+    return {
+      enumerable: true,
+      configurable: true
+      /* 이 외의 플래그도 반환할 수 있습니다. "value:..."도 가능합니다. */
+    };
   }
 
-})();
+});
+
+alert( Object.keys(user) ); // a, b, c
