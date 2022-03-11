ac7fdb31591310f446e33d9a408daf42534e0d54
2doKK <2dokyoung@gmail.com>
2022-03-11 15:30:44 +0900

    20220311

--- test.js
+++ test.js
@@ -1,10 +1,23 @@
 "use strict";
-function* generateSequence(start, end) {
-  for (let i = start; i <= end; i++) {
-    yield i;
-  }
-}
+let range = {
+  from: 1,
+  to: 5,
 
-for(let value of generateSequence(1, 5)) {
-  alert(value); // 1, then 2, then 3, then 4, then 5
-}
+  async *[Symbol.asyncIterator]() { // [Symbol.asyncIterator]: async function*()와 동일
+    for(let value = this.from; value <= this.to; value++) {
+
+      // 값 사이 사이에 약간의 공백을 줌
+      await new Promise(resolve => setTimeout(resolve, 1000));
+
+      yield value;
+    }
+  }
+};
+
+(async () => {
+
+  for await (let value of range) {
+    alert(value); // 1, 2, 3, 4, 5
+  }
+
+})();
