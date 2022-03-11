b5f166357d93c4c9a8cdebd6469fe8378718b1ea
2doKK <2dokyoung@gmail.com>
2022-03-11 14:22:50 +0900

    20220311

--- test.js
+++ test.js
@@ -14,3 +14,18 @@ let generator = gen();
 let question = generator.next().value;
 
 generator.throw(new Error("데이터베이스에서 답을 찾지 못했습니다.")); // (2)
+
+
+function* generate() {
+  let result = yield "2 + 2 = ?"; // Error in this line
+}
+
+let generator = generate();
+
+let question = generator.next().value;
+
+try {
+  generator.throw(new Error("데이터베이스에서 답을 찾지 못했습니다."));
+} catch(e) {
+  alert(e); // 에러 출력
+}
