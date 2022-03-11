3da6e5e06ea297af1cebc4cdc14a0b5feab3bf47
2doKK <2dokyoung@gmail.com>
2022-03-12 00:36:24 +0900

    20220312

--- test.js
+++ test.js
@@ -1,19 +1,14 @@
 "use strict";
-let user = { };
+let range = {
+  start: 1,
+  end: 10
+};
 
-user = new Proxy(user, {
-  ownKeys(target) { // 프로퍼티 리스트를 얻을 때 딱 한 번 호출됩니다.
-    return ['a', 'b', 'c'];
-  },
-
-  getOwnPropertyDescriptor(target, prop) { // 모든 프로퍼티를 대상으로 호출됩니다.
-    return {
-      enumerable: true,
-      configurable: true
-      /* 이 외의 플래그도 반환할 수 있습니다. "value:..."도 가능합니다. */
-    };
+range = new Proxy(range, {
+  has(target, prop) {
+    return prop >= target.start && prop <= target.end;
   }
-
 });
 
-alert( Object.keys(user) ); // a, b, c
+alert(5 in range); // true
+alert(50 in range); // false
