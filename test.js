57e38f169d28234a429c8c57c1c966268edfc43b
2doKK <2dokyoung@gmail.com>
2022-03-12 12:16:56 +0900

    20220312

--- test.js
+++ test.js
@@ -1,34 +1,10 @@
 "use strict";
-let handlers = Symbol('handlers');
+let a = 1;
 
-function makeObservable(target) {
-  // 1. 핸들러를 저장할 곳을 초기화합니다.
-  target[handlers] = [];
+function f() {
+  let a = 2;
 
-  // 나중에 호출될 것을 대비하여 핸들러 함수를 배열에 저장합니다.
-  target.observe = function(handler) {
-    this[handlers].push(handler);
-  };
-
-  // 2. 변경을 처리할 프락시를 만듭니다.
-  return new Proxy(target, {
-    set(target, property, value, receiver) {
-      let success = Reflect.set(...arguments); // 동작을 객체에 전달합니다.
-      if (success) { // 에러 없이 프로퍼티를 제대로 설정했으면
-        // 모든 핸들러를 호출합니다.
-        target[handlers].forEach(handler => handler(property, value));
-      }
-      return success;
-    }
-  });
+  eval('alert(a)'); // 2
 }
 
-let user = {};
-
-user = makeObservable(user);
-
-user.observe((key, value) => {
-  alert(`SET ${key}=${value}`);
-});
-
-user.name = "John";
+f();
