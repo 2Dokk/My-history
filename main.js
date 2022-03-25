08698ba89b052beaa0aab20ee355c03e3a53cd79
2doKK <2dokyoung@gmail.com>
2022-03-25 22:31:59 +0900

    20220325

--- main.js
+++ main.js
@@ -6,13 +6,8 @@ var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var title = queryData.id;
-    if(_url == '/'){
-      title = 'Welcome';
-    }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
-    response.writeHead(200);
+    
+    
     fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
       var template = `
       <!doctype html>
@@ -33,6 +28,7 @@ var app = http.createServer(function(request,response){
       </body>
       </html>
       `;
+      response.writeHead(200);
       response.end(template);
     })
  
