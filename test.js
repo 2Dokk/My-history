6ebbc7c344990a75616bfa463d6c50f9079c003a
2doKK <2dokyoung@gmail.com>
2022-03-28 01:14:24 +0900

    20220328

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
 let symmetry = 24;   
 
 let angle = 360 / symmetry;
-let clearButton, mouseButton, keyboardButton;
+let mouseButton, keyboardButton;
 let slider;
 let shadow = false;
 let changeMode = false;
+let setX = 0;
+let setY = 0;
 
 function setup() { 
   createCanvas(710, 710);
@@ -31,29 +33,43 @@ function draw() {
     drawingContext.shadowBlur = 0;
     drawingContext.shadowColor = color(207, 7, 99);
   }
-  print(shadow);
-  if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
-    let mx = mouseX - width / 2;
-    let my = mouseY - height / 2;
-    let pmx = pmouseX - width / 2;
-    let pmy = pmouseY - height / 2;
-    
-    if (mouseIsPressed) {
-      for (let i = 0; i < symmetry; i++) {
-        rotate(angle);
-        let sw = sizeSlider.value();
-        strokeWeight(sw);
-        stroke(mouseX,mouseY,50);
-        line(mx, my, pmx, pmy);
-        push();
-        scale(1, -1);
-        
-        line(mx, my, pmx, pmy);
-        pop();
+  if (changeMode){
+    if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
+      translate(setX,setY);
+      let mx = mouseX - width / 2;
+      let my = mouseY - height / 2;
+      let pmx = pmouseX - width / 2;
+      let pmy = pmouseY - height / 2;
+
+      if (mouseIsPressed) {
+        for (let i = 0; i < symmetry; i++) {
+          rotate(angle);
+          let sw = sizeSlider.value();
+          strokeWeight(sw);
+          stroke(mouseX,mouseY,50);
+          line(mx, my, pmx, pmy);
+          push();
+          scale(1, -1);
+
+          line(mx, my, pmx, pmy);
+          pop();
+        }
       }
+    }
+  } else{
+    if (mouseIsPressed) {
+      let sw = sizeSlider.value();
+      strokeWeight(sw);
+      stroke(0);
+      let mx = mouseX - width / 2;
+      let my = mouseY - height / 2;
+      let pmx = pmouseX - width / 2;
+      let pmy = pmouseY - height / 2;
+      line(mx, my, pmx, pmy);
     }
   }
 }
+
 function keyPressed() {
   if (keyCode === ENTER) {
     if (shadow){
@@ -66,6 +82,11 @@ function keyPressed() {
       changeMode = false;
     } else{
       changeMode = true;
+      }
+    } else if(keyCode === CONTROL){
+      if (mouseIsPressed){
+        setX = mouseX - width / 2;
+        setY = mouseY - height / 2;
       }
     }
 }
