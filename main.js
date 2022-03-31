3d5806842f1bdf4d1536a2ca345d833f5fc629f2
2doKK <2dokyoung@gmail.com>
2022-03-31 21:37:07 +0900

    20220331

--- main.js
+++ main.js
@@ -76,10 +76,12 @@ var app = http.createServer(function(request,response){
     } else if(pathname === '/create_process'){
         var body = '';
         request.on('data', function(data){
-          
+          body += data;  
         });
         request.on('end', function(){
-          
+          var post = qs.parse(body);
+          var title = post.title;
+          var description = post.description;
         });
         response.writeHead(200);
         response.end('Success');
