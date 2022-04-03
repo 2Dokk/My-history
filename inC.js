a04d42eeccafe0e3d4f7e41af902c417b650cce6
2doKK <2dokyoung@gmail.com>
2022-04-04 01:48:10 +0900

    20220403

--- inC.js
+++ inC.js
@@ -1,19 +1,26 @@
-let graph = [];
 function setup() {
-  createCanvas(400, 400);
-  for (let i = 0; i< 10; i ++){
-    for (let j = 0; j < 10; j++){
-      graph.push(["fill(255,0,0)"]);
+  createCanvas(windowWidth, windowHeight);
+  }
+  function draw() {
+    background(0);
+    randomSeed(0);
+    strokeWeight(3);
+    var x, y;
+    var delta = map(mouseX, 0, windowWidth, 10, 100);
+    for (y=0; y<windowHeight; y+=delta) {
+      for (x=0; x<windowWidth; x+=delta) {
+        var r = random(0, 1);
+        if (r < 0.5) {
+          var cX = map(x, 0, windowWidth, 0, 255);
+          var cY = map(y, 0, windowHeight, 0, 255);
+          stroke(cX,0,cY);
+          line(x, y, x+delta, y+delta);
+        } else {
+          var cX = map(x, 0, windowWidth, 0, 255);
+          var cY = map(y, 0, windowHeight, 0, 255);
+          stroke(cX,0,cY);
+          line(x+delta, y, x, y+delta);
+        }
+      }
     }
   }
-}
-
-function draw() {
-  background(220);
-    for (let i = 0; i< 10; i ++){
-      for (let j = 0; j < 10; j++){
-        eval(graph[i][j]);
-        rect(i*10,j*10,4,4);
-    }
-  }
-}
