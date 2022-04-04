57a9ac037989fb992ce33dcff5e4c09ac012a16e
2doKK <2dokyoung@gmail.com>
2022-04-04 20:49:20 +0900

    20220404

--- inC.js
+++ inC.js
@@ -4,9 +4,9 @@ function setup(){
   createCanvas(windowWidth, windowHeight);
   background(30);
   //angleMode(DEGREES);
-  //noiseDetail(1);
+  noiseDetail(1);
   
-  var density = 20;
+  var density = 10;
   var space = width / density;
   
   for (var x = 0; x < width; x += space){
@@ -23,16 +23,17 @@ function draw(){
   fill(255);
   
   for (var i =0; i< points.length; i++){
-    var r = map(points[i].x, 0, width, 50, 255);
-    var g = map(points[i].y, 0, height, 50 ,255);
-    var b = map(points[i].x, 0, width, 255, 50);
+    var r = map(points[i].x, 0, mouseX, 50, 230);
+    var g = map(points[i].y, 0, mouseX, 50, 230);
+    var b = map(points[i].x, 0, mouseY, 255, 100);
     
-    fill(r,g,b);
+    fill(255);
+    stroke(r,g,b);
     
     var angle = map(noise(points[i].x * mult, points[i].y * mult ), 0, 1, 0,720);
     
-    points[i].add(createVector(cos(angle), sin(angle)))
+    points[i].add(createVector(sin(angle), cos(angle)));
     
-    ellipse(points[i].x, points[i].y , 1);
+    ellipse(points[i].x, points[i].y , 5);
   }
 }
