30200f462002c3872e021c1d2b2de6d6e760680c
2doKK <2dokyoung@gmail.com>
2022-04-04 16:00:27 +0900

    20220403

--- inC.js
+++ inC.js
@@ -2,25 +2,38 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   }
   function draw() {
-    background(0);
+    background(255);
     randomSeed(0);
-    strokeWeight(3);
-    var x, y;
-    var delta = map(mouseX, 0, windowWidth, 10, 100);
+    let x, y, r;
+    let delta = 50;
+    let backSlashProb = map(mouseX, 0, windowWidth, 0,1);
+    let backSlashThickness = map(mouseY, 0, windowHeight,
+  3,30);
     for (y=0; y<windowHeight; y+=delta) {
       for (x=0; x<windowWidth; x+=delta) {
-        var r = random(0, 1);
-        if (r < 0.5) {
-          var cX = map(x, 0, windowWidth, 0, 255);
-          var cY = map(y, 0, windowHeight, 0, 255);
-          stroke(cX,0,cY);
+        r = random(0, 1);
+        if (r < backSlashProb) {
+          stroke(x, 0, y);
+          strokeWeight(backSlashThickness);
           line(x, y, x+delta, y+delta);
         } else {
-          var cX = map(x, 0, windowWidth, 0, 255);
-          var cY = map(y, 0, windowHeight, 0, 255);
-          stroke(cX,0,cY);
+          stroke(0);
+          strokeWeight(1);
           line(x+delta, y, x, y+delta);
         }
       }
     }
+    // stroke(255);
+    // strokeWeight(1);
+    // beginShape();
+    // vertex(windowWidth/2-30,windowHeight);
+    // vertex(windowWidth/2-30,windowHeight-300);
+    // vertex(windowWidth/2-100,windowHeight-200);
+    // vertex(windowWidth/2-150,windowHeight-250);
+    // vertex(windowWidth/2-30,windowHeight-300);
+    // vertex(windowWidth/2-30,windowHeight-300);
+    // vertex(windowWidth/2-30,windowHeight-300);
+    // vertex(windowWidth/2+30,windowHeight-300);
+    // vertex(windowWidth/2+30,windowHeight);
+    // endShape();
   }
