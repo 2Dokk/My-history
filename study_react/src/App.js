05f5478ce3b5b853bb4740cea972ac73696ef3d3
2doKK <2dokyoung@gmail.com>
2022-04-08 19:43:07 +0900

    20220408

--- study_react/src/App.js
+++ study_react/src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 
 function Header(props){
   return <header>
-  <h1><a herf="/">{props.title}</a></h1>
+  <h1><a herf="/" onClick={function(event) {
+    event.preventDefault();
+    props.onChangeMode();
+  }}>{props.title}</a></h1>
 </header>
 }
 function Article(props){
@@ -14,20 +17,30 @@ function Article(props){
 }
 
 function Nav(){
+  const lis = []
+  for (let i = 0; i<props.topics.length; i++){
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li> )
+  }
   return <nav>
   <ol>
-    <li><a href="/read/1">html</a></li>
-    <li><a href="/read/2">css</a></li>
-    <li><a href="/read/3">js</a></li>
+    {lis}
   </ol>
 </nav>
 }
 
 function App() {
+  const topics = [
+    {id:1, title: 'html', body: 'html is ...'},
+    {id:2, title: 'css', body: 'css is ...'},
+    {id:3, title: 'javascript', body: 'javascript is ...'}
+  ]
   return (
     <div>
-      <Header title="REACT"></Header>
-      <Nav></Nav>
+      <Header title="REACT" onChangeMode = {function(){
+        alert('Header');
+      }}></Header>
+      <Nav topics={topics}></Nav>
       <Article title="Welcome" body="Hello, WEB"></Article>
     </div>
 
