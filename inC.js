fb29541caf56eb8949ce23ed73474acbc78a0cc3
2doKK <2dokyoung@gmail.com>
2022-04-10 02:56:43 +0900

    202204010

--- inC.js
+++ inC.js
@@ -1,48 +1,52 @@
-const max_rainDrops = 1000;
-let rainDrops = [];
-let rainSound;
-class rainDrop {
-constructor(x, y, vy, sz, c) {
+const max_planets = 100;
+let planets = [];
+let backgroundMode = "none";
+function Planet(x, y, vx, vy, sz, c) {
 this.x = x;
 this.y = y;
+this.vx = vx;
 this.vy = vy;
 this.sz = sz;
 this.c = c;
-}
-move() {
+this.move = function() {
+// uniform velocity motion s_next = s_prev + v*t
+this.x += this.vx;
 this.y += this.vy;
-if (this.y > windowHeight) this.y = 0;
-if (mouseIsPressed) {
-let xdif = abs(this.x - mouseX);
-if (xdif < 100 + random(-100, 100)) {
-if ((this.y - mouseY) > random(-100, 100)) {
-this.y = 0;
+if (this.x<0 || this.x>windowWidth) this.vx = -this.vx;
+if (this.y<0 || this.y>windowHeight)     this.vy = -this.vy;
+if (this.y<0) this.y = -5
 }
-}
-}
-}
-render() {
+this.render = function() {
 noStroke();
 fill(this.c);
-ellipse(this.x, this.y, 2, this.sz);
+ellipse(this.x, this.y, this.sz, this.sz);
 }
 }
-function preload() {
-rainSound = loadSound('rain_inside_house.mp3');
-}
+
 function setup() {
 createCanvas(windowWidth, windowHeight);
-for (let i = 0; i < max_rainDrops; i++) {
-rainDrops[i] = new rainDrop(
+for (let i=0; i<max_planets; i++) {
+planets[i] = new Planet(
 random(0, windowWidth), random(0, windowHeight),
-random(30, 100),
-random(30, 100), color(random(100, 255)));
+random(-2, 2), random(-2, 2),
+random(30, 100), color(random(230,255),random(230,255) ,random(230,255)) );
 }
 }
+
 function draw() {
-background(0);
-for (let drop of rainDrops) {
-drop.move();
-drop.render();
+  if (backgroundMode == "none"){
+    background(163,230,250);
+  } else {
+    background(50);
+  }
+  for (let i=0; i<max_planets; i++) {
+    planets[i].move();
+    planets[i].render();
 }
+  if (mouseIsPressed === true){
+    backgroundMode = "rain";
+    for (var i = 0; i<max_planets; i++){
+      planets[i].c = color(40)
+    }
+  }
 }
