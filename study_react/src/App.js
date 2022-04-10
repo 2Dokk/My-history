0827539e42c10f56d0fcbd049f8793ec3d09f852
2doKK <2dokyoung@gmail.com>
2022-04-10 22:20:19 +0900

    202204010

--- study_react/src/App.js
+++ study_react/src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import Tweet from "./components/Tweet";
+import TweetList from "./components/TweetList";
 import CreateTweet from "./components/CreateTweet";
 
 function App() {
   const name = "Lee";
+  const message = "Welcome";
   return (
     <div>
       <div className="box">
         <h1>Hello</h1>
         <CreateTweet />
-        <Tweet />
+        <TweetList name={name} message={message} />
       </div>
       <h1>{name}</h1>
     </div>
