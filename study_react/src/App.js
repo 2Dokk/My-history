035d13c0d6abf2049f71fb671796757329931e1b
2doKK <2dokyoung@gmail.com>
2022-04-12 16:52:15 +0900

    202204010

--- study_react/src/App.js
+++ study_react/src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import Tweet from "./components/Tweet";
 import TweetList from "./components/TweetList";
 import CreateTweet from "./components/CreateTweet";
-import s from "./styles/App.module.css";
 
 function App() {
   const [name, setName] = useState("Lee");
@@ -11,7 +10,6 @@ function App() {
   const message = "Hello";
   return (
     <div>
-      <h1 className={s.title}>TWITTER LIGHT</h1>
       <div className="box">
         <CreateTweet
           textInput={textInput}
