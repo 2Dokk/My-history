27bc648a1c1d0db4a639ccb8bf3d429eda08c6cb
2doKK <2dokyoung@gmail.com>
2022-04-12 17:22:57 +0900

    202204010

--- study_react/src/App.js
+++ study_react/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import Tweet from "./components/Tweet";
 import TweetList from "./components/TweetList";
 import CreateTweet from "./components/CreateTweet";
