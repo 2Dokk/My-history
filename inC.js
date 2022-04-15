5f1481849ff8430d2267b61823840022213ba822
2doKK <2dokyoung@gmail.com>
2022-04-15 22:19:07 +0900

    20220415

--- inC.js
+++ inC.js
@@ -1,6 +1,7 @@
 var volhistory = [];
 let mic;
 let colorArray = [];
+let condArray = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,13 +13,29 @@ function setup() {
 function draw() {
   background(255);
   var vol = mic.getLevel();
-  print(vol);
   volhistory.push(vol);
-  colorArray.push(map(vol,0.0002,0.09,0,255))
   noStroke();
-  let fillColor = (0,Math.max(colorArray),Math.max(colorArray));
-  print(Math.max(colorArray));
-  fill(fillColor);
+  print(vol);
+  if (vol>0.01){
+    colorArray.push(map(vol,0.001,0.01,100,255));
+    condArray.push("blue");
+  } else if(vol > 0.1){
+            colorArray.push(map(vol,0.01,0.1,100,255));
+    condArray.push("green");
+            }else if(1){
+                     colorArray.push(map(vol,0.1,1,100,255));
+              condArray.push("red");
+                     }
+  if (condArray.includes("red")){
+     let index = condArray.indexOf("red");
+    fill(colorArray[index],0,0);
+     fill(0,Math.max(...colorArray),Math.max(...colorArray));
+  } else if (condArray.includes("green")){
+    let index = condArray.indexOf("green");
+    fill(0,colorArray[index],0);
+    fill(0,Math.max(...colorArray),Math.max(...colorArray));  
+             } else { fill(0,0,Math.max(...colorArray));
+}
   translate(width / 2, height / 2);
   beginShape();
   for (var i = 0; i < 360; i++) {
@@ -33,7 +50,10 @@ function draw() {
     volhistory.splice(0, 1);
   }
   
-  if (colorArray.length > 30) {
+  if (colorArray.length > 40) {
     colorArray.shift();
+  }
+    if (condArray.length > 40) {
+    condArray.shift();
   }
 }
