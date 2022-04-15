1caf7f5aa9a914c731eb5efa16031fca62373617
2doKK <2dokyoung@gmail.com>
2022-04-15 21:24:31 +0900

    20220415

--- inC.js
+++ inC.js
@@ -1,34 +1,39 @@
-var amp;
 var volhistory = [];
-
+let mic;
+let colorArray = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
   mic = new p5.AudioIn();
   mic.start();
-  
-  amp = new p5.Amplitude();
 }
 
 function draw() {
-  background(0);
+  background(255);
   var vol = mic.getLevel();
+  print(vol);
   volhistory.push(vol);
-  stroke(255);
-  noFill();
-
+  colorArray.push(map(vol,0.0002,0.09,0,255))
+  noStroke();
+  let fillColor = (0,Math.max(colorArray),Math.max(colorArray));
+  print(Math.max(colorArray));
+  fill(fillColor);
   translate(width / 2, height / 2);
   beginShape();
   for (var i = 0; i < 360; i++) {
     var r = map(volhistory[i], 0, 1, 10, 100);
     var x = r * cos(i) * 10;
-    var y = r * sin(i)* 10;
+    var y = r * sin(i) * 10;
     vertex(x, y);
   }
   endShape();
 
   if (volhistory.length > 360) {
     volhistory.splice(0, 1);
+  }
+  
+  if (colorArray.length > 30) {
+    colorArray.shift();
   }
 }
