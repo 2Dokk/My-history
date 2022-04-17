f74526aa39a7f36aa6a3eec05ec7e92da6ce374a
2doKK <2dokyoung@gmail.com>
2022-04-18 00:34:50 +0900

    20220417

--- inC.js
+++ inC.js
@@ -1,5 +1,4 @@
 let mic, fft;
-let result = 1000;
 let checkIncline = true;
 
 function setup() {
@@ -13,35 +12,44 @@ function setup() {
 }
 
 function draw() {
-  let saveValue = [];
+  let result = 1000;
   let saveIndex = [];
   let savePoint = [];
   let indexValue = [];
   background(200);
 
   let spectrum = fft.analyze();
-  for (var i = 1; i < 1024; i++) {
+  for (var i = 1; i < spectrum.length; i++) {
     if (spectrum[i] - spectrum[i - 1] >= 0) {
       checkIncline = true;
     } else if (spectrum[i] - spectrum[i - 1] < 0 && checkIncline == true) {
-      checkInline = false;
+      checkIncline = false;
       savePoint.push(i - 1);
     } else {
-      checkInline = false;
+      checkIncline = false;
     }
   }
+  for (let i = 1; i < savePoint.length; i++) {
+    let m = savePoint[i] - savePoint[i - 1];
+    if (m < 10) {
+      savePoint.splice(i - 1, 1);
+    }
+  }
+  print(savePoint);
   for (let i = 0; i < savePoint.length; i++) {
     indexValue.push(spectrum[savePoint[i]]);
   }
   for (let i = 0; i < 10; i++) {
     let max = Math.max(...indexValue);
-    saveValue.push(max);
     let ind = indexValue.indexOf(max);
     indexValue.filter((v) => v !== max);
     saveIndex.push(savePoint[ind]);
     savePoint.splice(ind, 1);
   }
-  saveIndex.sort();
+  saveIndex.sort(function (a, b) {
+    return a - b;
+  });
+  print("saveIndex :", saveIndex);
   for (let i = 1; i < 10; i++) {
     if (abs(saveIndex[i] - saveIndex[i - 1]) < result) {
       result = abs(saveIndex[i] - saveIndex[i - 1]);
