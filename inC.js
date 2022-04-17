e168e278fdc8220e2e994ac7b9f9276eb2fd1666
2doKK <2dokyoung@gmail.com>
2022-04-17 15:47:09 +0900

    20220417

--- inC.js
+++ inC.js
@@ -16,13 +16,13 @@ class petal {
     this.y = y;
     this.szw = szw;
     this.szh = szh;
-    this.col = color;
+    this.color = color;
     this.angle = angle;
   }
 
   render() {
     noStroke();
-    fill(this.col);
+    fill(this.color);
     push();
     rotate(this.angle);
     ellipse(this.x, this.y, this.szw, this.szh);
@@ -48,8 +48,8 @@ function draw() {
   push();
   translate(windowWidth / 2, windowHeight / 2);
   background(220);
-  var vol = mic.getLevel();
-  var spectrum = fft.analyze();
+  let vol = mic.getLevel();
+  let spectrum = fft.analyze();
 
   if (flower.length == 0) {
     i = 0;
@@ -66,13 +66,16 @@ function draw() {
   //set color
   let index = spectrum.lastIndexOf(1);
   if (index / 255 > 3) {
-    flower[i] = new petal(0, y, szW, sz, (index, 0, 250 - index), angle);    flower[i].col = (0, index, 250 - index);
+    flower[i] = new petal(0, y, szW, sz, (index, 0, 250 - index), angle);    flower[i].color = (index % 255, 0, 250 - index % 255);
+    print(flower[i].color);
   } else if (index / 225 > 2) {
     flower[i] = new petal(0, y, szW, sz, 0, angle);
-    flower[i].col = (0, index, 250 - index);
+    flower[i].color = (0, index % 255, 250 - index % 255);
+    print(flower[i].color);
   } else {
     flower[i] = new petal(0, y, szW, sz, (0, 0, index % 255), angle);
-        flower[i].col = (0, 0, index % 255);
+    flower[i].color = (0, 0, index % 255);
+    print(flower[i].color);
   }
   angle += ((360 / number) * szW) / 10;
   if (angle > 360) {
@@ -81,9 +84,10 @@ function draw() {
   for (let l = flowers.length - 1; l >= 0; l--)
     for (let flo of flowers[l]) {
       flo.render();
-      print(flo.col);
+      //print(flo.color);
     }
   noStroke();
   fill(255, 255, 0);
   ellipse(0, 0, 20, 20);
+  pop();
 }
