cd521fedc345f1b24070aa94bf85585d44b9a833
2doKK <2dokyoung@gmail.com>
2022-04-17 23:12:42 +0900

    20220417

--- inC.js
+++ inC.js
@@ -1,119 +1,51 @@
-let mic;
-let amp;
-let fft;
-let i;
-let angle = 0;
-let color;
-let szH = 30;
-let szW = 10;
-let number = 20;
-let r, g, b;
-let sz;
-let setSize = 20;
-
-let flowers = [];
-let flower = [];
-class petal {
-  constructor(x, y, szw, szh, r, g, b, angle) {
-    this.x = x;
-    this.y = y;
-    this.szw = szw;
-    this.szh = szh;
-    this.angle = angle;
-    this.r = r;
-    this.g = g;
-    this.b = b;
-  }
-
-  render() {
-    noStroke();
-    fill(this.r, this.g, this.b);
-    push();
-    rotate(this.angle);
-    ellipse(this.x, this.y, this.szw, this.szh);
-    pop();
-  }
-}
+let mic, fft;
+let result = 1000;
+let checkIncline = true;
 
 function setup() {
-  frameRate(5);
-  angleMode(DEGREES);
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(710, 400);
+  noFill();
+
   mic = new p5.AudioIn();
   mic.start();
-
-  amp = new p5.Amplitude();
   fft = new p5.FFT();
   fft.setInput(mic);
-
-  randomSeed(0);
 }
 
 function draw() {
-  push();
-  translate(windowWidth / 2, windowHeight / 2);
-  background(220);
-  let vol = mic.getLevel();
+  let saveValue = [];
+  let saveIndex = [];
+  let savePoint = [];
+  let indexValue = [];
+  background(200);
+
   let spectrum = fft.analyze();
-
-  if (flower.length == 0) {
-    i = 0;
-    angle = 0;
-    flowers[flowers.length] = flower;
-    szH += 30;
-    szW += 5;
-    number += 20;
-    //set size
-  } else i = flower.length;
-  if (vol > 0.1) {
-    sz = map(vol, 0.01, 0.3, szH + setSize * 2, szH + setSize * 3);
-    print(sz);
-  } else if (vol > 0.01) {
-    sz = map(vol, 0.001, 0.1, szH + setSize, szH + setSize * 2);
-    print(sz);
-  } else if (vol > 0.001) {
-    sz = map(vol, 0.0001, 0.01, szH, szH + setSize);
-    print(sz);
-  }
-  var y = -szH / 2;
-
-  //set color
-  let index = spectrum.lastIndexOf(1);
-  if (index / 255 > 3) {
-    flower[i] = new petal(
-      0,
-      y,
-      szW,
-      sz,
-      index % 255,
-      0,
-      250 - (index % 255),
-      angle
-    );
-  } else if (index / 225 > 2) {
-    flower[i] = new petal(
-      0,
-      y,
-      szW,
-      sz,
-      0,
-      index % 255,
-      250 - (index % 255),
-      angle
-    );
-  } else {
-    flower[i] = new petal(0, y, szW, sz, 0, 0, index % 255, angle);
-  }
-  angle += ((360 / number) * szW) / 10;
-  if (angle > 360) {
-    flower = [];
-  }
-  for (let l = flowers.length - 1; l >= 0; l--)
-    for (let flo of flowers[l]) {
-      flo.render();
+  for (var i = 1; i < 1024; i++) {
+    if (spectrum[i] - spectrum[i - 1] >= 0) {
+      checkIncline = true;
+    } else if (spectrum[i] - spectrum[i - 1] < 0 && checkIncline == true) {
+      checkInline = false;
+      savePoint.push(i - 1);
+    } else {
+      checkInline = false;
     }
-  noStroke();
-  fill(255, 255, 0);
-  ellipse(0, 0, 20, 20);
-  pop();
+  }
+  for (let i = 0; i < savePoint.length; i++) {
+    indexValue.push(spectrum[savePoint[i]]);
+  }
+  for (let i = 0; i < 10; i++) {
+    let max = Math.max(...indexValue);
+    saveValue.push(max);
+    let ind = indexValue.indexOf(max);
+    indexValue.filter((v) => v !== max);
+    saveIndex.push(savePoint[ind]);
+    savePoint.splice(ind, 1);
+  }
+  saveIndex.sort();
+  for (let i = 1; i < 10; i++) {
+    if (abs(saveIndex[i] - saveIndex[i - 1]) < result) {
+      result = abs(saveIndex[i] - saveIndex[i - 1]);
+    }
+  }
+  print(result);
 }
