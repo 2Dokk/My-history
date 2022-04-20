3abb2c864784e9bf314a3147729d67d67b15fd0e
2doKK <2dokyoung@gmail.com>
2022-04-21 03:18:49 +0900

    20220420

--- go.c
+++ go.c
@@ -1,13 +1,43 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
+int Add(int a, int b);
+int Sub(int a, int b);
+int Mul(int a, int b);
+float Div(int a, int b);
 
 int main(void)
 {
-	int range;
-	srand(time(NULL));
-	range = (6 - 1) + 1;
-	printf("\nThe random number is %d\n", rand() % range * 3 + 1);
+	int num1, num2;
+
+	printf("First num : ");
+	scanf("%d", &num1);
+	printf("Second num : ");
+	scanf("%d", &num2);
+
+	printf("Add : %d\n", Add(num1, num2));
+	printf("Sub : %d\n", Sub(num1, num2));
+	printf("Mul : %d\n", Mul(num1, num2));
+	printf("Div : %f\n", Div(num1, num2));
 
 	return 0;
+}
+
+int Add(int a, int b)
+{
+	return a + b;
+}
+
+int Sub(int a, int b)
+{
+	return a - b;
+}
+
+int Mul(int a, int b)
+{
+	return a * b;
+}
+
+float Div(int a, int b)
+{
+	return (float)a / (float)b;
 }
