a0cc18073b9005c63d42cefbf33bc3280c92f009
2doKK <2dokyoung@gmail.com>
2022-04-21 03:56:03 +0900

    20220421

--- go.c
+++ go.c
@@ -1,77 +1,15 @@
 #include <stdio.h>
 
-void Celsius();
-void GetCelsius(float* a);
-float CelsiustoFahrenheit(float a);
-void PrintCelsius(float a);
+int main(void) {
 
-void Fahrenheit();
-void GetFahrenheit(float* a);
-float FahrenheittoCelsius(float a);
-void PrintFahrenheit(float a);
-
-int main(void)
-{
-	Celsius();
-	Fahrenheit();
-
-	return 0;
-}
-
-void Celsius()
-{
-	float Ctemp, Ftemp;
-
-	GetCelsius(&Ctemp);
-	Ftemp = CelsiustoFahrenheit(Ctemp);
-	PrintCelsius(Ftemp);
+	int num;
+	int sum = 0;
+	printf("Enter your numbers: <EOF> to stop.\n");
+	while (scanf("%d", &num) != EOF) {
+		sum += num;
+		printf("Total: %3d\n", sum);
+	}
+	printf("\n\n*** End of Program ***\n");
 	
-	return;
-}
-
-void GetCelsius(float* a)
-{
-	printf("\n���� �µ��� �Է��Ͻÿ� : ");
-	scanf("%f", a);
-	return;
-}
-
-float CelsiustoFahrenheit(float a)
-{
-	return a * 9.0f / 5.0f + 32.0f;
-}
-
-void PrintCelsius(float a)
-{
-	printf("ȭ�� �µ��� %.2f �Դϴ�\n", a);
-	return;
-}
-
-void Fahrenheit()
-{
-	float Ftemp, Ctemp;
-
-	GetFahrenheit(&Ftemp);
-	Ctemp = FahrenheittoCelsius(Ftemp);
-	PrintFahrenheit(Ctemp);
-
-	return;
-}
-
-void GetFahrenheit(float* a)
-{
-	printf("\nȭ�� �µ��� �Է��Ͻÿ� : ");
-	scanf("%f", a);
-	return;
-}
-
-float FahrenheittoCelsius(float a)
-{
-	return (a - 32.0f) * 5.0f / 9.0f;
-}
-
-void PrintFahrenheit(float a)
-{
-	printf("���� �µ��� %.2f �Դϴ�\n", a);
-	return;
+	return 0;
 }
