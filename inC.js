03247b1eeb67464061f3624d66453c8a20b4b378
2Dokk <2dokyoung@gmail.com>
2022-06-05 04:05:47 +0900

    20220605

--- inC.js
+++ inC.js
@@ -1,12 +1,14 @@
 let weatherData;
 let region = "London";
 let weather;
+let state = "Loading";
+let key = "037e877babc2441ebbe170923220406";
+let url = `https://cors-anywhere.herokuapp.com/http://api.weatherapi.com/v1/current.json?key=${key}&q=${region}`;
 function preload() {
-  let url = `https://cors-anywhere.herokuapp.com/http://api.weatherapi.com/v1/current.json?key=037e877babc2441ebbe170923220406&q=${region}`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => (weather = data.current.condition.text))
-    .then(() => console.log(weatherData))
+    .then(() => (state = "Done"))
     .catch((error) => console.log("error:", error));
 }
 
@@ -17,11 +19,24 @@ function setup() {
 
 function draw() {
   background(220);
+  if (weather == "Sunny") {
+  }
+
+  if (state == "Loading") {
+  } else if (state == "Done") {
+  }
 }
 
 function keyPressed() {
   if (keyCode === ENTER) {
     region = prompt("어디 날씨가 궁금하신가요?", "");
+    state = "Loading";
+    url = `https://cors-anywhere.herokuapp.com/http://api.weatherapi.com/v1/current.json?key=${key}&q=${region}`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => (weather = data.current.condition.text))
+      .then(() => (state = "Done"))
+      .catch((error) => console.log("error:", error));
     console.log(weather);
   }
 }
