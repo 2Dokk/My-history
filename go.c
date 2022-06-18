77878b47e15a630138cfbc47156e010ace1c5d62
2Dokk <2dokyoung@gmail.com>
2022-06-18 18:47:50 +0900

    20220618

--- go.c
+++ go.c
@@ -1,40 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
+//enum week {SUN , MON, TUE, WED, THU, FRI, SAT};
 
-int printPerson(struct person p);
-
-typedef struct{
-    char name[20];
-    char mail[20];
-    int mobile;
-} PROFESSOR;
-typedef struct{
-    char name[20];
-    char major[20];
-    int ID;
-    float cgpa;
-} STUDENT;
-typedef struct {
-    char type;
-    union {
-        PROFESSOR prof;
-        STUDENT stu;
-    }u;
-}PERSON;
-PERSON person;
 
 int main(void){
-    PROFESSOR person1;
-    STUDENT person2;
-    person1.name = "james";
-    person1.mail = "james@hanmail.net";
-    person1.mobile = 1097063456;
-    person2.name = "david";
-    person2.major = "conputer science";
-    person2.id = 20010123;
-    person2.cgpa = 3.10;
-};
-
-int printPerson(struct person p){
     
 }
--- inC.js
+++ inC.js
@@ -5,6 +5,7 @@ var leaves = [];
 var ground, treeTop;
 var firstClick = true;
 var img;
+let screen = "smallTree";
 
 function preload() {
   img = loadImage("Norang.png");
@@ -24,86 +25,91 @@ function setup() {
 }
 
 function draw() {
-  drawingContext.shadowBlur = 0;
+  if (screen === "smallTree") {
+    drawingContext.shadowBlur = 0;
 
-  //SEEDS
-  strokeWeight(4);
-  stroke(0);
-  fill(255);
-  seeds.forEach((seed, index, arr) => {
-    seed.y += 3;
-    if (seed.y > ground) {
-      //transform seed in tree
-      arr.splice(index, 1);
-      trees.push(createTree(seed.x, ground));
-    } else {
-      fill("#65402B");
-      ellipse(seed.x, seed.y, 15, 15);
-    }
-  });
-
-  //BRANCHS
-  noStroke();
-  trees.forEach((tree, index, arr) => {
-    tree.dir += (noise(tree.phase + millis() / 100) - 0.5) / 3;
-    tree.dir += ((PI - tree.dir) * 0.09) / (tree.generation + 1); //point north
-    tree.pos.x += sin(tree.dir) * 2;
-    tree.pos.y += cos(tree.dir) * 2;
+    //SEEDS
+    strokeWeight(4);
+    stroke(0);
     fill(255);
-    fill("#845A38");
-    ellipse(tree.pos.x, tree.pos.y, tree.radius * 2, tree.radius * 2);
-
-    //shadow the branch
-    fill(0);
-    for (var i = 0; i < PI; i += PI / 10) {
-      let x = cos(i) * tree.radius;
-      let y = sin(i) * tree.radius;
-      ellipse(tree.pos.x + x, tree.pos.y + y, i, i);
-    }
-
-    tree.radius *= 0.998 / (tree.generation / 300 + 1);
-
-    tree.life--;
-    if (tree.life < 0) {
-      arr.splice(index, 1);
-      if (tree.radius > 3) {
-        //transform root in branchs
-        trees.push(createTree(tree.pos.x, tree.pos.y, tree));
-        trees.push(createTree(tree.pos.x, tree.pos.y, tree));
+    seeds.forEach((seed, index, arr) => {
+      seed.y += 3;
+      if (seed.y > ground) {
+        //transform seed in tree
+        arr.splice(index, 1);
+        trees.push(createTree(seed.x, ground));
       } else {
-        //transform final branch in leaves
-        leaves.push(createLeaf(tree.pos.x, tree.pos.y));
+        fill("#65402B");
+        ellipse(seed.x, seed.y, 15, 15);
       }
-    }
-  });
+    });
 
-  //LEAVES
-  noStroke();
-  drawingContext.shadowColor = color(0, 0, 0, 100);
-  drawingContext.shadowBlur = 5;
-  let w = width / 2 - img.width / 2;
-  leaves.forEach((leaf, index, arr) => {
-    var x = leaf.pos.x + random(-50, 50);
-    var y = leaf.pos.y + random(-50, 50);
-    fill(
-      img.get(mod(floor(x - w), img.width), mod(floor(y - treeTop), img.height))
-    );
-    //fill(random(50), random(100, 200), random(50));
-    var size = random(2, 7);
-    push();
-    translate(x, y);
-    rotate(random(TWO_PI));
-    rect(0, 0, size, size, 0, size / 2, 0, size / 2);
-    pop();
+    //BRANCHS
+    noStroke();
+    trees.forEach((tree, index, arr) => {
+      tree.dir += (noise(tree.phase + millis() / 100) - 0.5) / 3;
+      tree.dir += ((PI - tree.dir) * 0.09) / (tree.generation + 1); //point north
+      tree.pos.x += sin(tree.dir) * 2;
+      tree.pos.y += cos(tree.dir) * 2;
+      fill(255);
+      fill("#845A38");
+      ellipse(tree.pos.x, tree.pos.y, tree.radius * 2, tree.radius * 2);
 
-    leaf.life--;
-    if (leaf.life < 0) {
-      arr.splice(index, 1);
-    }
-  });
-  noStroke();
-  fill("#B3855A");
-  rect(0, ground, windowWidth, windowHeight - ground);
+      //shadow the branch
+      fill(0);
+      for (var i = 0; i < PI; i += PI / 10) {
+        let x = cos(i) * tree.radius;
+        let y = sin(i) * tree.radius;
+        ellipse(tree.pos.x + x, tree.pos.y + y, i, i);
+      }
+
+      tree.radius *= 0.998 / (tree.generation / 300 + 1);
+
+      tree.life--;
+      if (tree.life < 0) {
+        arr.splice(index, 1);
+        if (tree.radius > 3) {
+          //transform root in branchs
+          trees.push(createTree(tree.pos.x, tree.pos.y, tree));
+          trees.push(createTree(tree.pos.x, tree.pos.y, tree));
+        } else {
+          //transform final branch in leaves
+          leaves.push(createLeaf(tree.pos.x, tree.pos.y));
+        }
+      }
+    });
+
+    //LEAVES
+    noStroke();
+    drawingContext.shadowColor = color(0, 0, 0, 100);
+    drawingContext.shadowBlur = 5;
+    let w = width / 2 - img.width / 2;
+    leaves.forEach((leaf, index, arr) => {
+      var x = leaf.pos.x + random(-50, 50);
+      var y = leaf.pos.y + random(-50, 50);
+      fill(
+        img.get(
+          mod(floor(x - w), img.width),
+          mod(floor(y - treeTop), img.height)
+        )
+      );
+      //fill(random(50), random(100, 200), random(50));
+      var size = random(2, 7);
+      push();
+      translate(x, y);
+      rotate(random(TWO_PI));
+      rect(0, 0, size, size, 0, size / 2, 0, size / 2);
+      pop();
+
+      leaf.life--;
+      if (leaf.life < 0) {
+        arr.splice(index, 1);
+      }
+    });
+    noStroke();
+    fill("#B3855A");
+    rect(0, ground, windowWidth, windowHeight - ground);
+  }
 }
 
 function mousePressed() {
@@ -141,10 +147,17 @@ function createTree(x, y, root) {
 function createLeaf(x, y) {
   return {
     pos: createVector(x, y),
-    life: random(20, 70),
+    life: random(200, 250),
   };
 }
 
 function mod(m, n) {
   return ((m % n) + n) % n;
+}
+
+function keyPressed() {
+  if (keyCode == ENTER) {
+    createCanvas(windowWidth, windowHeight);
+    screen = "bigTree";
+  }
 }
